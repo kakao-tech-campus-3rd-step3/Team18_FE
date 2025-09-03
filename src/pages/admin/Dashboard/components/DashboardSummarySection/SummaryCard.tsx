57f0e4c37e94df1c88c74579dffe03a72c1d3d42
kanghaeun <xgkg0330@jnu.ac.kr>
2025-09-04 01:20:35 +0900

    feat: 대시보드 요약 카드 컴포넌트(SummaryCard) 추가

---
+++ src/pages/admin/Dashboard/components/DashboardSummarySection/SummaryCard.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import type { DashboardCard } from '.';
+
+type Props = Omit<DashboardCard, 'id'>;
+
+export const SummaryCard = ({ label, value, image }: Props) => {
+  return (
+    <Wrapper>
+      <IconWrapper>{image}</IconWrapper>
+      <TextWrapper>
+        <Label>{label}</Label>
+        <Value>{value}</Value>
+      </TextWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div(({ theme }) => ({
+  width: 'auto',
+  height: '7.9rem',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '3.6rem',
+  padding: '0 3.4rem',
+  background: theme.colors.bg,
+  borderRadius: theme.radius.lg,
+}));
+
+const IconWrapper = styled.div(({ theme }) => ({
+  color: theme.colors.gray900,
+}));
+
+const TextWrapper = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.85rem',
+});
+
+const Label = styled.p(({ theme }) => ({
+  fontSize: '1.35rem',
+  color: theme.colors.gray900,
+}));
+
+const Value = styled.p(({ theme }) => ({
+  fontSize: '2.2rem',
+  fontWeight: theme.font.weight.bold,
+  color: theme.colors.gray900,
+}));
