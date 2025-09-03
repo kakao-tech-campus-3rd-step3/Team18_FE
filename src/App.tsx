2e9ecc58ec6a4ab0e9330b5c05af0c0468c4c7ce
kanghaeun <xgkg0330@jnu.ac.kr>
2025-09-03 22:57:40 +0900

    feat: 대시보드 페이지 라우트 추가 및 기본 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Routes } from './pages/Routes';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div/>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </>
   );
 }
 
