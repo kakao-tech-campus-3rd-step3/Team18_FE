dec47fa1826c4eaf5bdf9fff431873d83f32d0bf
Ajin <ajin@Ajinui-noteubug.local>
2025-09-03 18:32:52 +0800

    refactor: ThemeProvider로감싸기 및 emotion.d.ts 타입 선언 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Routes } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './styles/theme';
 
 function App() {
   return (
-    <>
-      <Routes></Routes>
-    </>
+    <ThemeProvider theme={theme}>
+      <div/>
+    </ThemeProvider>
   );
 }
 
