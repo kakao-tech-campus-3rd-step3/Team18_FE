815b88d7400e9fbac7690ee50f0c1de361c1079a
aaaaaattt <choigur03@gamil.com>
2025-09-05 05:07:32 +0900

    feat: 임시 라우터 추가 - 초기 페이지 = 메인 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/main/MainPage';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './styles/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div/>
+      <Routes>
+        <Route path='/' element={<MainPage />}></Route>
+      </Routes>
     </ThemeProvider>
   );
 }
