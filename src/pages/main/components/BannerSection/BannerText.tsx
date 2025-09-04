c3119861689b868e29efef456a792e7509dc1e85
aaaaaattt <choigur03@gamil.com>
2025-09-05 04:53:35 +0900

    feat: 동아리 배너 부분 구현(UI) - BannerText, BannerWrapper, ClubSearchInput (Styled compnent) 추가

---
+++ src/pages/main/components/BannerSection/BannerText.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const BannerText = styled.div(({ theme }) => ({
+  fontSize: 20,
+  fontWeight: theme.font.weight.bold,
+  color: theme.colors.textPrimary,
+}));
