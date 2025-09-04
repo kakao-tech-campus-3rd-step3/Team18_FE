b93a6674d64ca2ebde012953ddfdbff0a4a123bb
aaaaaattt <choigur03@gamil.com>
2025-09-05 05:04:49 +0900

    feat: 동아리 목록 구현(UI) - StyledComponent 추가

---
+++ src/pages/main/components/ClubListSection/index.tsx
@@ -0,0 +1,37 @@
+import { useClub } from '@/pages/main/hook/useClub';
+import { Grid } from '@/pages/main/components/ClubListSection/ClubGrid';
+import { ClubItem } from '@/pages/main/components/ClubListSection/ClubItem';
+import {
+  ClubCateogryText,
+  ClubIntroduction,
+  ClubNameText,
+  RecruitStatusBox,
+  RecruitStatusText,
+} from './ClubInfo';
+
+const ClubListSection = () => {
+  const { clubs, isLoading, error } = useClub();
+
+  return (
+    <>
+      {isLoading && <div>로딩중입니다...</div>}
+      <Grid>
+        {clubs?.map((club) => (
+          <ClubItem key={club.id}>
+            <ClubCateogryText>{club.category}</ClubCateogryText>
+
+            <ClubNameText>{club.name}</ClubNameText>
+            <ClubIntroduction>{club.short_Introduction}</ClubIntroduction>
+            <RecruitStatusBox status={club.recruit_status}>
+              <RecruitStatusText status={club.recruit_status}>
+                {club.recruit_status}
+              </RecruitStatusText>
+            </RecruitStatusBox>
+          </ClubItem>
+        ))}
+      </Grid>
+    </>
+  );
+};
+
+export default ClubListSection;
