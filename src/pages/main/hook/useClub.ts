e0307ad026eba2092b03d9d223f6760c9188d7df
aaaaaattt <choigur03@gamil.com>
2025-09-05 04:56:29 +0900

    feat: 동아리 목록 api 호출 - 커스텀훅 분리 - Response type 추가

---
+++ src/pages/main/hook/useClub.ts
@@ -0,0 +1,26 @@
+import { getClubs } from '@/pages/main/api/club';
+import { useEffect, useState } from 'react';
+import type { Club } from '@/pages/main/domain/club';
+
+export const useClub = () => {
+  const [clubs, setClubs] = useState<Club[]>();
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await getClubs();
+        setClubs(response.clubs);
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { clubs, isLoading, error };
+};
