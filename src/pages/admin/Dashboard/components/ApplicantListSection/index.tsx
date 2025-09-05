e02955f5d2e557bae4dce2290745bbd59e19f646
aaaaaattt <choigur03@gamil.com>
2025-09-05 15:53:01 +0900

    Merge branch 'develop' into feature/mainpage-club-list#7

---
+++ src/pages/admin/Dashboard/components/ApplicantListSection/index.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { ApplicationStatusFilter } from './ApplicationFilter';
+import { ApplicantList } from './ApplicantList';
+import type { ApplicationFilterOption } from '@/types/dashboard';
+
+export const ApplicantListSection = () => {
+  const [filterOption, setFilterOption] = useState<ApplicationFilterOption>('전체');
+
+  return (
+    <>
+      <ApplicantFilterTopBarWrapper>
+        <PageTitle>지원자 관리</PageTitle>
+        <ApplicationStatusFilter option={filterOption} onOptionChange={setFilterOption} />
+      </ApplicantFilterTopBarWrapper>
+      <ListWrapper>
+        <ApplicantList />
+      </ListWrapper>
+    </>
+  );
+};
+
+const ApplicantFilterTopBarWrapper = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginTop: '4.5rem',
+  padding: '0 2rem',
+});
+
+const PageTitle = styled.h1(({ theme }) => ({
+  fontSize: theme.font.size.xl,
+  fontWeight: theme.font.weight.bold,
+  color: theme.colors.textPrimary,
+}));
+
+const ListWrapper = styled.main(({ theme }) => ({
+  backgroundColor: theme.colors.bg,
+  minHeight: 'auto',
+  padding: '2.6rem 3rem',
+  borderRadius: theme.radius.lg,
+}));
