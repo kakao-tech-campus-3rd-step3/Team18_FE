22b8200f253b704160e7a195208f69e1d8240813
aaaaaattt <choigur03@gamil.com>
2025-09-08 19:57:04 +0900

    refactor: 메인페이지 Banner 부분 styled component 통합 - Banner.style.tsx 파일에 통합

--- src/pages/user/Main/components/BannerSection/index.tsx
+++ src/pages/user/Main/components/BannerSection/index.tsx
@@ -1,14 +1,12 @@
-import { BannerText } from '@/pages/user/Main/components/BannerSection/BannerText';
-import { BannerWrapper } from '@/pages/user/Main/components/BannerSection/BannerWrapper';
-import ClubSearchInput from '@/pages/user/Main/components/BannerSection/ClubSearchInput';
+import ClubSearchInputStyle from '@/pages/user/Main/components/BannerSection/ClubSearchInput.style.tsx';
+import { BannerWrapper,BannerText } from '@/pages/user/Main/components/BannerSection/Banner.style.tsx';
 
-const BannerSection = () => {
+
+export const BannerSection = () => {
   return (
     <BannerWrapper>
       <BannerText>{` 동아리움 (Dongari-um): 동아리 + 공간(-um), 동아리들을 위한 공간.`}</BannerText>
-      <ClubSearchInput />
+      <ClubSearchInputStyle />
     </BannerWrapper>
   );
 };
-
-export default BannerSection;
