3638edecee38f965cde90d63150b6058cc989a8d
aaaaaattt <choigur03@gamil.com>
2025-09-09 18:44:01 +0900

    chore: component 이름 변경

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import { DashboardPage } from '@/pages/admin/Dashboard/Page';
-import Page from '@/pages/user/Main/Page.tsx';
+import { MainPage } from '@/pages/user/Main/Page.tsx';
 
 export const Routes = () => {
   return (
     <RouterRoutes>
       <Route path={ROUTE_PATH.DASHBOARD} element={<DashboardPage />} />
-      <Route path={ROUTE_PATH.MAIN} element={<Page />} />
+      <Route path={ROUTE_PATH.MAIN} element={<MainPage />} />
     </RouterRoutes>
   );
 };
