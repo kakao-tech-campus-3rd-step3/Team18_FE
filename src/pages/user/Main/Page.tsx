49f666b5c075dc24336d867fcdacba8d6a87f092
aaaaaattt <choigur03@gamil.com>
2025-09-09 18:40:13 +0900

    chore: named export 방식 통일

--- src/pages/user/Main/Page.tsx
+++ src/pages/user/Main/Page.tsx
@@ -2,7 +2,7 @@ import ClubListSection from '@/pages/user/Main/components/ClubListSection';
 import { BannerSection } from '@/pages/user/Main/components/BannerSection';
 import ClubCategorySection from '@/pages/user/Main/components/ClubCategorySection';
 
-const MainPage = () => {
+export const MainPage = () => {
   return (
     <>
       <BannerSection />
@@ -11,5 +11,3 @@ const MainPage = () => {
     </>
   );
 };
-
-export default MainPage;
