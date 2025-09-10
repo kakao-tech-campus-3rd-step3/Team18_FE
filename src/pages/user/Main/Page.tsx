3a899d6e3e17a8e67e6d083fbf4de7ccc07e13f2
aaaaaattt <choigur03@gamil.com>
2025-09-10 16:28:29 +0900

    feat: 동아리 카테고리 필터링 UI 추가

--- src/pages/user/Main/Page.tsx
+++ src/pages/user/Main/Page.tsx
@@ -1,12 +1,15 @@
 import { ClubListSection } from '@/pages/user/Main/components/ClubListSection';
 import { BannerSection } from '@/pages/user/Main/components/BannerSection';
 import { ClubCategorySection } from '@/pages/user/Main/components/ClubCategorySection';
+import { useState } from 'react';
+import type { ClubCategory } from '@/pages/user/Main/constant/clubCategory.ts';
 
 export const MainPage = () => {
+  const [filter, setFilter] = useState<ClubCategory>('전체');
   return (
     <>
       <BannerSection />
-      <ClubCategorySection />
+      <ClubCategorySection selected={filter} onSelect={(c: ClubCategory) => setFilter(c)} />
       <ClubListSection />
     </>
   );
