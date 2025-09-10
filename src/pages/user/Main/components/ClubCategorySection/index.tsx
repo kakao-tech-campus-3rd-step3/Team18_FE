1ff668ed0f773823b69f409da5d75a1abd4f4b11
aaaaaattt <choigur03@gamil.com>
2025-09-10 16:28:29 +0900

    feat: 동아리 카테고리 필터링 UI 추가

--- src/pages/user/Main/components/ClubCategorySection/index.tsx
+++ src/pages/user/Main/components/ClubCategorySection/index.tsx
@@ -1,15 +1,28 @@
-import { CLUB_CATEGORY } from '@/pages/user/Main/constant/clubCategory';
+import { CLUB_CATEGORY, type ClubCategory } from '@/pages/user/Main/constant/clubCategory';
 import {
   CategoryTabContainer,
   CategoryTab,
 } from '@/pages/user/Main/components/ClubCategorySection/index.styled.tsx';
 
-export const ClubCategorySection = () => {
+type Props = {
+  onSelect: (category: ClubCategory) => void;
+  selected?: ClubCategory;
+};
+
+export const ClubCategorySection = ({ onSelect, selected }: Props) => {
   return (
     <>
       <CategoryTabContainer>
         {CLUB_CATEGORY.map((category) => (
-          <CategoryTab key={category}>{category}</CategoryTab>
+          <CategoryTab
+            key={category}
+            onClick={() => {
+              onSelect(category);
+            }}
+            selected={selected === category}
+          >
+            {category}
+          </CategoryTab>
         ))}
       </CategoryTabContainer>
     </>
