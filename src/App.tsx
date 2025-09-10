b94744d2e7ba61ec026186884c4f972c17f1aee4
aaaaaattt <choigur03@gamil.com>
2025-09-10 12:28:42 +0900

    refactor: 전체 동아리 목록 react-query로 상태 관리

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { Routes } from './pages/Routes';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from '@/styles/theme';
 import { Navigation } from '@/shared/components/Navigation';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <Navigation role='president' />
         <Routes />
       </ThemeProvider>
-    </>
+    </QueryClientProvider>
   );
 }
 
