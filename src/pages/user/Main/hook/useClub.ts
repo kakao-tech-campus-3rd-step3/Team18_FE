b54eb8e4f6bff9b8605cf72bf33854fa272cf85c
aaaaaattt <choigur03@gamil.com>
2025-09-10 17:51:17 +0900

    refactor: 동아리목록 api 함수 수정 - filter 매개변수 추가

--- src/pages/user/Main/hook/useClub.ts
+++ src/pages/user/Main/hook/useClub.ts
@@ -1,12 +1,12 @@
-import { getClubs } from '@/pages/user/Main/api/club.ts';
-
+import { getClubsByCategory } from '@/pages/user/Main/api/club.ts';
 import { useQuery } from '@tanstack/react-query';
 import type { ClubResponse } from '@/types/club.ts';
+import type { ClubCategory } from '@/pages/user/Main/constant/clubCategory.ts';
 
-export const useClub = () => {
+export const useClub = (filter: ClubCategory) => {
   const { data, error, isLoading } = useQuery<ClubResponse>({
-    queryKey: ['clubData'],
-    queryFn: getClubs,
+    queryKey: ['clubData', filter],
+    queryFn: () => getClubsByCategory(filter),
   });
 
   return { data, isLoading, error };
