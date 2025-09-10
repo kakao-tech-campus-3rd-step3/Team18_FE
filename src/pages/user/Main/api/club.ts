ab0fbb2fc5ccf58d8cf9c3aaaca2996daf904ae9
aaaaaattt <choigur03@gamil.com>
2025-09-10 17:49:54 +0900

    refactor: 동아리목록 api 함수 수정 - filter 매개변수 추가

--- src/pages/user/Main/api/club.ts
+++ src/pages/user/Main/api/club.ts
@@ -1,6 +1,9 @@
 import type { ClubResponse } from '@/types/club.ts';
+import type { ClubCategory } from '@/pages/user/Main/constant/clubCategory.ts';
 
-export async function getClubs(): Promise<ClubResponse> {
-  const response = await fetch('/api/clubs');
+//REST API ->  /api/clubs?category={...}
+
+export async function getClubsByCategory(filter: ClubCategory): Promise<ClubResponse> {
+  const response = await fetch(`/api/clubs/search/category?categoryName=${filter}`);
   return await response.json();
 }
