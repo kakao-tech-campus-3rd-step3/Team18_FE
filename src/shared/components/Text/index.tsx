21220272d680043569c0e8533f23eb16260e670d
kanghaeun <xgkg0330@jnu.ac.kr>
2025-09-11 21:26:06 +0900

    feat: 공통 텍스트 컴포넌트 구현

---
+++ src/shared/components/Text/index.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import { theme } from '@/styles/theme';
+import React from 'react';
+
+interface TextProps {
+  size?: FontSizeKey;
+  weight?: FontWeightKey;
+  children: React.ReactNode;
+  color?: string;
+}
+
+export const Text = ({
+  size = 'base',
+  weight = 'regular',
+  color = '#000000',
+  children,
+}: TextProps) => {
+  return (
+    <StyledText size={size} weight={weight} color={color}>
+      {children}
+    </StyledText>
+  );
+};
+
+const StyledText = styled.div<{ size: FontSizeKey; weight: FontWeightKey; color: string }>`
+  font-size: ${({ theme, size }) => theme.font.size[size]};
+  font-weight: ${({ theme, weight }) => theme.font.weight[weight]};
+  color: ${({ color }) => color};
+`;
+
+type FontSizeKey = keyof typeof theme.font.size;
+type FontWeightKey = keyof typeof theme.font.weight;
