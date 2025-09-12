ec20d9e16daaa2385edaddcde5840d733bf6f647
kanghaeun <xgkg0330@jnu.ac.kr>
2025-09-12 13:45:15 +0900

    feat: 지원자 상태값(합격, 불합격, 미정) 변경 기능 구현

---
+++ src/pages/admin/ApplicationDetail/components/ApplicantProfileSection/ApplicantStatusButton.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled';
+import type { ApplicantData } from '@/types/dashboard';
+import type { ApplicantStatus } from '@/types/dashboard';
+
+type Props = {
+  label: ApplicantData['status'];
+  value: ApplicantStatus;
+  selected: boolean;
+  onClick: (status: ApplicantStatus) => void;
+};
+
+export const ApplicantStatusButton = ({ label, value, selected, onClick }: Props) => {
+  return (
+    <Wrapper label={label} selected={selected} onClick={() => onClick(value)}>
+      {label}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.button<Pick<Props, 'selected' | 'label'>>(({ theme, selected, label }) => {
+  const statusStyles = {
+    합격: {
+      backgroundColor: theme.colors.primary100,
+      color: theme.colors.primary800,
+    },
+    불합격: {
+      backgroundColor: theme.colors.red100,
+      color: theme.colors.red600,
+    },
+    미정: {
+      backgroundColor: theme.colors.gray100,
+      color: theme.colors.gray600,
+    },
+  };
+
+  const defaultStyle = {
+    backgroundColor: theme.colors.gray100,
+    color: theme.colors.gray600,
+  };
+
+  const appliedStyle = selected ? statusStyles[label] : defaultStyle;
+
+  return {
+    border: 'none',
+    padding: '0.4rem 1.7rem',
+    cursor: 'pointer',
+    fontSize: theme.font.size.sm,
+    fontWeight: selected ? theme.font.weight.bold : theme.font.weight.regular,
+    transition: 'all 200ms ease-in-out',
+    borderRadius: '4rem',
+    ...appliedStyle,
+  };
+});
