9cd966f6f11107582071d2c9834841966f33d72f
aaaaaattt <choigur03@gamil.com>
2025-09-14 19:41:12 +0900

    refactor : 일관된 인터페이스를 통해 내부 구현 캡슐화

--- src/pages/user/Main/hook/useClub.ts
+++ src/pages/user/Main/hook/useClub.ts
@@ -1,11 +1,15 @@
-import { getClubsByCategory } from '@/pages/user/Main/api/club.ts';
+import { type ClubResponse, getClubsByCategory } from '@/pages/user/Main/api/club.ts';
 import { useQuery } from '@tanstack/react-query';
-import type { ClubResponse } from '@/types/club.ts';
 import type { ClubCategory } from '@/pages/user/Main/constant/clubCategory.ts';
 
 export const useClub = (filter: ClubCategory) => {
-  return useQuery<ClubResponse>({
+  const result = useQuery<ClubResponse>({
     queryKey: ['clubData', filter],
     queryFn: () => getClubsByCategory(filter),
   });
+  return {
+    clubs: result.data?.clubs,
+    error: result.error,
+    isLoading: result.isLoading,
+  };
 };
