5cf9e21bb30ae0b7cb13ef24f805ca5309cc8e07
aaaaaattt <choigur03@gamil.com>
2025-09-14 21:07:19 +0900

    chore : lint test 관련 에러 수정 - 주석 추가 - 타입 추가(Theme) - 공백 제거

--- src/shared/components/SectionTitle/index.tsx
+++ src/shared/components/SectionTitle/index.tsx
@@ -1,10 +1,10 @@
 import styled from '@emotion/styled';
 
 export const SectionTitle = styled.h2(({ theme }) => ({
-  display: 'block', 
-  width: '100%',  
-  fontSize: theme.font.size.lg, 
-  fontWeight: theme.font.weight.bold, 
+  display: 'block',
+  width: '100%',
+  fontSize: theme.font.size.lg,
+  fontWeight: theme.font.weight.bold,
   margin: '1.5rem 0 0 0',
   color: theme.colors.textPrimary,
 }));
