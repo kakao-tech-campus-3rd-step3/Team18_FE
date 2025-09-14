b5ef47d7d1155e7829d8520d8e5563078312d01e
ganimjeong <ganimjeong@gmail.com>
2025-09-14 17:06:20 +0800

    feat:sidebar섹션  mockData 연동

--- src/pages/user/ClubDetail/components/ClubInfoSidebarSection/index.tsx
+++ src/pages/user/ClubDetail/components/ClubInfoSidebarSection/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Button } from '@/shared/components/Button';
+import { mockClubDetail } from '../mock';
 
 const ClubInfoSidebarSection: React.FC = () => {
+  const { 
+    presidentName,
+    presidentPhoneNumber,
+    location,
+    recruitStart,
+    recruitEnd,
+    regularMeetingInfo,
+    recruitStatus
+  } = mockClubDetail;
+
+  const formatDate = (dateStr: string) => {
+    const date = new Date(dateStr);
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
+  };
+
   return (
     <SidebarContainer>
-      <InfoItem>회장 이름: 홍길동</InfoItem>
-      <InfoItem>연락처: 010-1234-5678</InfoItem>
-      <InfoItem>동방 위치: 123호</InfoItem>
-      <InfoItem>모집 기간: 9/1 ~ 9/30</InfoItem>
-      <InfoItem>정기 모임: 매주 월요일</InfoItem>
+      <InfoItem>회장 이름: {presidentName}</InfoItem>
+      <InfoItem>연락처: {presidentPhoneNumber}</InfoItem>
+      <InfoItem>동방 위치: {location}</InfoItem>
+      <InfoItem>
+        모집 기간: {formatDate(recruitStart)} ~ {formatDate(recruitEnd)}
+      </InfoItem>
+      <InfoItem>정기 모임: {regularMeetingInfo}</InfoItem>
+      <InfoItem>모집 상태: {recruitStatus}</InfoItem>
       <Button to='/'>지원하기</Button>
       <Notice>지원 시 유의사항이 여기에 들어갑니다.</Notice>
     </SidebarContainer>
