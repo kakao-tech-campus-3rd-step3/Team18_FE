6bcaf65e9d9364382aeeff61dfa84a9b57263c35
aaaaaattt <choigur03@gamil.com>
2025-09-14 20:32:00 +0900

    chore : useClub hook 결과값 interface 추가

--- src/pages/user/Main/hook/useClub.ts
+++ src/pages/user/Main/hook/useClub.ts
@@ -1,8 +1,15 @@
 import { type ClubResponse, getClubsByCategory } from '@/pages/user/Main/api/club.ts';
 import { useQuery } from '@tanstack/react-query';
 import type { ClubCategory } from '@/pages/user/Main/constant/clubCategory.ts';
+import type { Club } from '@/types/club.ts';
 
-export const useClub = (filter: ClubCategory) => {
+export interface UseClubResult {
+  clubs?: Club[];
+  error?: Error | null;
+  isLoading: boolean;
+}
+
+export const useClub = (filter: ClubCategory): UseClubResult => {
   const result = useQuery<ClubResponse>({
     queryKey: ['clubData', filter],
     queryFn: () => getClubsByCategory(filter),
