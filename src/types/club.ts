be78d116653c9ba1b3d2369ad79a2102707073bd
aaaaaattt <choigur03@gamil.com>
2025-09-14 19:42:02 +0900

    chore : ClubResponse type 이동 - types -> Main/api

--- src/types/club.ts
+++ src/types/club.ts
@@ -4,11 +4,6 @@ export type Club = {
   id: number;
   name: string;
   category: string;
-  short_Introduction: string;
-  recruit_status: RecruitStatus;
-};
-
-export type ClubResponse = {
-  clubs: Club[];
-  //pagination 관련 값들...(논의 후)
+  shortIntroduction: string;
+  recruitStatus: RecruitStatus;
 };
