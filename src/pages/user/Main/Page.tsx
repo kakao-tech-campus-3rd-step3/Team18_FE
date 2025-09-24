229c473912be556e2a34c0c4791a5dd2dde470d0
aaaaaattt <choigur03@gamil.com>
2025-09-25 00:00:29 +0900

    refactor:  category eng version 추가

--- src/pages/user/Main/Page.tsx
+++ src/pages/user/Main/Page.tsx
@@ -2,13 +2,13 @@ import { ClubListSection } from '@/pages/user/Main/components/ClubListSection';
 import { BannerSection } from '@/pages/user/Main/components/BannerSection';
 import { MemoizedClubCategorySection } from '@/pages/user/Main/components/ClubCategorySection';
 import { useCallback, useState } from 'react';
-import type { ClubCategory } from '@/pages/user/Main/constant/clubCategory.ts';
+import type { ClubCategoryEng } from '@/pages/user/Main/constant/clubCategory.ts';
 
 export const MainPage = () => {
-  const [filter, setFilter] = useState<ClubCategory>('전체');
+  const [filter, setFilter] = useState<ClubCategoryEng>('all');
   const [searchText, setSearchText] = useState('');
 
-  const handleFilter = useCallback((c: ClubCategory) => {
+  const handleFilter = useCallback((c: ClubCategoryEng) => {
     setFilter(c);
   }, []);
 
