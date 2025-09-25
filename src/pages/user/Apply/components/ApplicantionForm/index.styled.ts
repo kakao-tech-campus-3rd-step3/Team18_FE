470a523605ebce167202b2beb4cd628f72971ffd
kanghaeun <xgkg0330@jnu.ac.kr>
2025-09-26 05:52:57 +0900

    feat: OutlineInputField, OutlineTextareaField 컴포넌트 공통 스타일 연동

--- src/pages/user/Apply/components/ApplicantionForm/index.styled.ts
+++ src/pages/user/Apply/components/ApplicantionForm/index.styled.ts
@@ -1,21 +1,5 @@
 import styled from '@emotion/styled';
 
-type InputProps = {
-  width?: string;
-  height?: string;
-  padding?: string;
-};
-
-export const TextInput = styled.input<InputProps>(({ theme, width, height, padding }) => ({
-  borderRadius: '10px',
-  border: `1px solid ${theme.colors.gray200}`,
-  backgroundColor: theme.colors.gray100,
-  width: width || '100%',
-  height: height || '30px',
-  padding: padding || '0 10px',
-  textAlign: 'left',
-}));
-
 type OptionInputProps = {
   type: 'checkbox' | 'radio';
 };
@@ -31,14 +15,4 @@ export const OptionInput = styled.input<OptionInputProps>(({ theme, type }) => (
     backgroundColor: theme.colors.primary,
     borderColor: theme.colors.primary,
   },
-}));
-
-export const TextAreaInput = styled.textarea<InputProps>(({ theme, width, height, padding }) => ({
-  borderRadius: '10px',
-  border: `1px solid ${theme.colors.gray200}`,
-  backgroundColor: theme.colors.gray100,
-  width: width || '100%',
-  height: height || '30px',
-  padding: padding || '10px 10px',
-  textAlign: 'left',
 }));
