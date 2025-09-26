87642f6ed04aaffcc951cfefd764c72fac213288
kanghaeun <xgkg0330@jnu.ac.kr>
2025-09-26 11:54:29 +0900

    style: 지원서 상세페이지 반응형 적용
    - 72.5rem 이하에서 댓글을 지원서 하단으로 이동

--- src/pages/admin/ApplicationDetail/Page.tsx
+++ src/pages/admin/ApplicationDetail/Page.tsx
@@ -1,10 +1,10 @@
-import styled from '@emotion/styled';
 import { ApplicantProfileSection } from './components/ApplicantProfileSection/index';
 import { ApplicantInfoSection } from './components/ApplicantInfoSection';
 import { ApplicantQuestionSection } from './components/ApplicationQuestionSection';
 import { CommentSection } from './components/CommentSection';
 import { useDetailApplications } from './hooks/useDetailApplication';
 import { useParams } from 'react-router-dom';
+import * as S from './index.styled';
 
 export const ApplicationDetailPage = () => {
   const { clubId, applicantId } = useParams();
@@ -20,8 +20,8 @@ export const ApplicationDetailPage = () => {
   if (error) return <div>에러발생 : {error.message}</div>;
 
   return (
-    <Layout>
-      <ApplicationDetailView>
+    <S.PageLayout>
+      <S.MainContent>
         <ApplicantProfileSection
           name={detailApplicants?.applicantInfo.name}
           department={detailApplicants?.applicantInfo.department}
@@ -37,53 +37,10 @@ export const ApplicationDetailPage = () => {
         <ApplicantQuestionSection
           questionsAndAnswers={detailApplicants?.questionsAndAnswers || []}
         />
-      </ApplicationDetailView>
-      <CommentView>
+      </S.MainContent>
+      <S.AsideContent>
         <CommentSection />
-      </CommentView>
-    </Layout>
+      </S.AsideContent>
+    </S.PageLayout>
   );
 };
-
-const Layout = styled.main(({ theme }) => ({
-  backgroundColor: theme.colors.bgBlue,
-  height: '100vh',
-  display: 'flex',
-  flexDirection: 'row',
-
-  '@media (max-width: 60.72669rem)': {
-    flexDirection: 'column',
-  },
-}));
-
-const ApplicationDetailView = styled.section(({ theme }) => ({
-  backgroundColor: theme.colors.bg,
-  width: '66%',
-  height: 'calc(100vh - 5rem)',
-  padding: '45px',
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '32px',
-
-  minWidth: 0,
-
-  '@media (max-width: 60.72669rem)': {
-    width: '100%',
-    flex: 'none',
-  },
-}));
-
-const CommentView = styled.section(({ theme }) => ({
-  width: '34%',
-  backgroundColor: theme.colors.bg,
-  borderRadius: '2rem 2rem 0 0',
-  padding: '24px',
-  margin: '4rem 2rem 0 2rem',
-  flexShrink: 0,
-
-  '@media (max-width: 60.72669rem)': {
-    width: '100%',
-    minHeight: '300px',
-    marginRight: '5rem',
-  },
-}));
