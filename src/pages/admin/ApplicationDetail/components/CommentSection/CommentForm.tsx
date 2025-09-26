20974d5618349509761a2ac95fad796cf1014eba
kanghaeun <xgkg0330@jnu.ac.kr>
2025-09-26 10:45:24 +0900

    style: 새로운 댓글을 작성할 수 있는 CommentForm 구현

---
+++ src/pages/admin/ApplicationDetail/components/CommentSection/CommentForm.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import { OutlineTextareaField } from '@/shared/components/Form/TextareaField/OutlineTextareaField';
+import { Text } from '@/shared/components/Text';
+
+export const CommentForm = () => {
+  return (
+    <Layout>
+      <Wrapper>
+        <Text weight={'medium'}>댓글</Text>
+      </Wrapper>
+      <OutlineTextareaField />
+    </Layout>
+  );
+};
+
+const Layout = styled.div(({}) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.8rem',
+  marginBottom: '3rem',
+}));
+
+const Wrapper = styled.div(() => ({
+  display: 'flex',
+  gap: '16px',
+}));
