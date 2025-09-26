98beb0dbd632d2cff51c7aa4af9aca0862b5b841
Ajin <ajin@Ajinui-noteubug.local>
2025-09-26 17:09:56 +0800

    refactor: 라우팅주소 복수로 통일

--- src/constants/routerPath.ts
+++ src/constants/routerPath.ts
@@ -2,7 +2,7 @@ export const ROUTE_PATH = {
   DASHBOARD: 'club/dashboard',
   APPLICATIONDETAIL: 'clubs/:clubId/applicants/:applicantId',
   MAIN: '/',
-  CLUBDETAIL: 'club/:clubId',
+  CLUBDETAIL: 'clubs/:clubId',
   CLUBEDIT: 'clubs/:clubId/edit',
   CLUBAPPLICATION: 'club/:id/apply',
 };
