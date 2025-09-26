9a6ad9708bece0d2338a9926c9cfd2848aaa7966
ganimjeong <ganimjeong@gmail.com>
2025-09-26 17:10:13 +0800

    refactor: 라우팅주소 복수로 통일

--- src/constants/routerPath.ts
+++ src/constants/routerPath.ts
@@ -1,7 +1,7 @@
 export const ROUTE_PATH = {
+  MAIN: '/',
   DASHBOARD: 'club/dashboard',
   APPLICATIONDETAIL: 'clubs/:clubId/applicants/:applicantId',
-  MAIN: '/',
   CLUBDETAIL: 'clubs/:clubId',
   CLUBEDIT: 'clubs/:clubId/edit',
   CLUBAPPLICATION: 'club/:id/apply',
