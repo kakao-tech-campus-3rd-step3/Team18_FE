a2d32c9720ae8311d8f2e119806d5288ea96301e
ganimjeong <ganimjeong@gmail.com>
2025-09-27 17:26:11 +0800

    refactor: 글씨와 inline 요소 사용할 수 있도록 수정

--- src/shared/components/SectionTitle/index.tsx
+++ src/shared/components/SectionTitle/index.tsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 
 export const SectionTitle = styled.h2(({ theme }) => ({
-  display: 'block',
-  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
   fontSize: theme.font.size.lg,
   fontWeight: theme.font.weight.bold,
   margin: '1.5rem 0 0 0',
   color: theme.colors.textPrimary,
+  width: '100%',
+  gap: '0.5rem',
 }));
