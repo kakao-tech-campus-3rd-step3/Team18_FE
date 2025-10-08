b100a14feab56b2c3dbde02240cb2be98202dc52
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-08 15:45:59 +0900

    fix: 동아리 정보 수정 폼 에러 메시지 위치 수정
    - InfoItem의 row 레이아웃으로 인해 에러 메시지가 input 옆에 표시되던 문제 해결
    - InputWrapper 컴포넌트 추가하여 에러 메시지가 input 하단에 표시되도록 수정

--- src/pages/admin/ClubDetailEdit/components/ClubInfoSidebarEditSection/index.tsx
+++ src/pages/admin/ClubDetailEdit/components/ClubInfoSidebarEditSection/index.tsx
@@ -25,101 +25,115 @@ export const ClubInfoSidebarEditSection = () => {
     <S.SidebarContainer>
       <S.InfoItem>
         <S.Label required>회장 이름:</S.Label>
-        <UnderlineInputField
-          {...register('presidentName', {
-            required: '회장 이름을 입력해주세요.',
-            maxLength: { value: 20, message: '20자 이하로 입력해주세요.' },
-          })}
-          invalid={!!errors.presidentName}
-          message={errors.presidentName?.message}
-        />
+        <S.InputWrapper>
+          <UnderlineInputField
+            {...register('presidentName', {
+              required: '회장 이름을 입력해주세요.',
+              maxLength: { value: 20, message: '20자 이하로 입력해주세요.' },
+            })}
+            invalid={!!errors.presidentName}
+            message={errors.presidentName?.message}
+          />
+        </S.InputWrapper>
       </S.InfoItem>
 
       <S.InfoItem>
         <S.Label>연락처:</S.Label>
-        <UnderlineInputField
-          {...register('presidentPhoneNumber', {
-            pattern: {
-              value: /^\d{3}-\d{4}-\d{4}$/,
-              message: '전화번호 형식은 000-0000-0000입니다.',
-            },
-            validate: (value) =>
-              !value || /^\d{3}-\d{4}-\d{4}$/.test(value) || '전화번호 형식은 000-0000-0000입니다.',
-          })}
-          invalid={!!errors.presidentPhoneNumber}
-          message={errors.presidentPhoneNumber?.message}
-        />
+        <S.InputWrapper>
+          <UnderlineInputField
+            {...register('presidentPhoneNumber', {
+              pattern: {
+                value: /^\d{3}-\d{4}-\d{4}$/,
+                message: '’ - ’를 포함한 형식으로 입력해주세요.',
+              },
+              validate: (value) =>
+                !value ||
+                /^\d{3}-\d{4}-\d{4}$/.test(value) ||
+                '전화번호 형식은 000-0000-0000입니다.',
+            })}
+            invalid={!!errors.presidentPhoneNumber}
+            message={errors.presidentPhoneNumber?.message}
+          />
+        </S.InputWrapper>
       </S.InfoItem>
 
       <S.InfoItem>
         <S.Label required>동방 위치:</S.Label>
-        <UnderlineInputField
-          {...register('location', {
-            required: '동방 위치를 입력해주세요.',
-            maxLength: { value: 20, message: '20자 이하로 입력해주세요.' },
-          })}
-          invalid={!!errors.location}
-          message={errors.location?.message}
-        />
+        <S.InputWrapper>
+          <UnderlineInputField
+            {...register('location', {
+              required: '동방 위치를 입력해주세요.',
+              maxLength: { value: 20, message: '20자 이하로 입력해주세요.' },
+            })}
+            invalid={!!errors.location}
+            message={errors.location?.message}
+          />
+        </S.InputWrapper>
       </S.InfoItem>
 
       <S.InfoItem>
         <S.Label>모집 시작일:</S.Label>
-        <Controller
-          control={control}
-          name='recruitStart'
-          rules={{ required: '모집 시작일을 선택해주세요.' }}
-          render={({ field, fieldState }) => (
-            <DatePicker
-              selected={field.value ?? undefined}
-              onChange={(date) => field.onChange(date)}
-              onBlur={field.onBlur}
-              dateFormat='yyyy/MM/dd'
-              customInput={
-                <UnderlineInputField
-                  invalid={!!fieldState.error}
-                  message={fieldState.error?.message}
-                />
-              }
-            />
-          )}
-        />
+        <S.InputWrapper>
+          <Controller
+            control={control}
+            name='recruitStart'
+            rules={{ required: '모집 시작일을 선택해주세요.' }}
+            render={({ field, fieldState }) => (
+              <DatePicker
+                selected={field.value ?? undefined}
+                onChange={(date) => field.onChange(date)}
+                onBlur={field.onBlur}
+                dateFormat='yyyy/MM/dd'
+                customInput={
+                  <UnderlineInputField
+                    invalid={!!fieldState.error}
+                    message={fieldState.error?.message}
+                  />
+                }
+              />
+            )}
+          />
+        </S.InputWrapper>
       </S.InfoItem>
 
       <S.InfoItem>
         <S.Label>모집 마감일:</S.Label>
-        <Controller
-          control={control}
-          name='recruitEnd'
-          rules={{ required: '모집 마감일을 선택해주세요.' }}
-          render={({ field, fieldState }) => (
-            <DatePicker
-              selected={field.value ?? undefined}
-              onChange={(date) => field.onChange(date)}
-              onBlur={field.onBlur}
-              minDate={getValues('recruitStart') ?? undefined}
-              dateFormat='yyyy/MM/dd'
-              customInput={
-                <UnderlineInputField
-                  invalid={!!fieldState.error}
-                  message={fieldState.error?.message}
-                />
-              }
-            />
-          )}
-        />
+        <S.InputWrapper>
+          <Controller
+            control={control}
+            name='recruitEnd'
+            rules={{ required: '모집 마감일을 선택해주세요.' }}
+            render={({ field, fieldState }) => (
+              <DatePicker
+                selected={field.value ?? undefined}
+                onChange={(date) => field.onChange(date)}
+                onBlur={field.onBlur}
+                minDate={getValues('recruitStart') ?? undefined}
+                dateFormat='yyyy/MM/dd'
+                customInput={
+                  <UnderlineInputField
+                    invalid={!!fieldState.error}
+                    message={fieldState.error?.message}
+                  />
+                }
+              />
+            )}
+          />
+        </S.InputWrapper>
       </S.InfoItem>
 
       <S.InfoItem>
         <S.Label required>정기 모임:</S.Label>
-        <UnderlineInputField
-          {...register('regularMeetingInfo', {
-            required: '정기 모임 정보를 입력해주세요.',
-            maxLength: { value: 50, message: '50자 이하로 입력해주세요.' },
-          })}
-          invalid={!!errors.regularMeetingInfo}
-          message={errors.regularMeetingInfo?.message}
-        />
+        <S.InputWrapper>
+          <UnderlineInputField
+            {...register('regularMeetingInfo', {
+              required: '정기 모임 정보를 입력해주세요.',
+              maxLength: { value: 50, message: '50자 이하로 입력해주세요.' },
+            })}
+            invalid={!!errors.regularMeetingInfo}
+            message={errors.regularMeetingInfo?.message}
+          />
+        </S.InputWrapper>
       </S.InfoItem>
 
       <S.InfoItem column>
