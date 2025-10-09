63e6ad58c00ff8c911b3a7abe093d92af4b0e313
aaaaaattt <choigur03@gamil.com>
2025-10-09 21:55:04 +0900

    chore: category 상수 수정

--- src/pages/user/Main/constant/clubCategory.ts
+++ src/pages/user/Main/constant/clubCategory.ts
@@ -1,5 +1,5 @@
 export type ClubCategory = '전체' | '문예' | '학술' | '종교' | '체육' | '봉사';
 
-export type ClubCategoryEng = 'ALL' | 'ARTS' | 'ACADEMICS' | 'RELIGION' | 'SPORTS' | 'VOLUNTEER';
+export type ClubCategoryEng = 'ALL' | 'STUDY' | 'LITERATURE' | 'RELIGION' | 'SPORTS' | 'VOLUNTEER';
 
 export const CLUB_CATEGORY: ClubCategory[] = ['전체', '문예', '학술', '종교', '체육', '봉사'];
