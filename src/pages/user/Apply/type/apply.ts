cfe35fcbf9e52f6507ee9eac799d7b73bbcb9504
aaaaaattt <choigur03@gamil.com>
2025-10-09 23:46:55 +0900

    chore: Question type 속성명 재변경

--- src/pages/user/Apply/type/apply.ts
+++ src/pages/user/Apply/type/apply.ts
@@ -15,7 +15,7 @@ export type Question =
       questionType: typeof QuestionTypes.TIME_SLOT;
       question: string;
       required: boolean;
-      timeSlotOption: InterviewSchedule[];
+      timeSlotOptions: InterviewSchedule[];
     }
   | {
       questionNum: number;
