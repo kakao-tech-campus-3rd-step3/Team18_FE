1e051112284550291c08e18ba867aa2aa8bf55cb
ganimjeong <ganimjeong@gmail.com>
2025-10-11 15:17:18 +0800

    feat: Navigation에서 to를 계산해 넘기게 함

--- src/shared/components/Navigation/index.tsx
+++ src/shared/components/Navigation/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { NAV_CONFIG } from '@/constants/navigation';
+import { replaceRouteParams } from '@/utils/replaceRouteParams'; // ✅ 추가
 import { NavigationContainer } from './NavigationContainer';
 import { NavigationItem } from './NavigationItem';
 import type { Role } from '@/types/navigation';
@@ -12,6 +13,9 @@ export const Navigation = ({ role }: NavbarProps) => {
   const [currentRoute, setCurrentRoute] = useState('동아리움');
   const items = NAV_CONFIG[role];
 
+  // TODO: 추후 context나 로그인 정보에서 가져오기
+  const clubId = '1'; // 임시 값
+
   const handleItemClick = (label: React.ReactNode) => {
     if (typeof label === 'string') {
       setCurrentRoute(label);
@@ -20,17 +24,23 @@ export const Navigation = ({ role }: NavbarProps) => {
 
   return (
     <NavigationContainer selectedItem={currentRoute}>
-      {items.map((item) => (
-        <NavigationItem
-          key={item.key}
-          to={item.to}
-          isLogo={item.isLogo}
-          selected={currentRoute === item.label}
-          onClick={handleItemClick}
-        >
-          {item.label}
-        </NavigationItem>
-      ))}
+      {items.map((item) => {
+        const path = item.to?.includes(':clubId')
+          ? replaceRouteParams(item.to, { clubId })
+          : item.to;
+
+        return (
+          <NavigationItem
+            key={item.key}
+            to={path}
+            isLogo={item.isLogo}
+            selected={currentRoute === item.label}
+            onClick={handleItemClick}
+          >
+            {item.label}
+          </NavigationItem>
+        );
+      })}
     </NavigationContainer>
   );
 };
