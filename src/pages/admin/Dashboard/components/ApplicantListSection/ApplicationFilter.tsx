adde828a8fe606932fd4be35d6a0149e6de8694b
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-12 03:24:08 +0900

    feat: 상태별(전체,합격,불합격,미정) 카운트 계산 로직 추가

--- src/pages/admin/Dashboard/components/ApplicantListSection/ApplicationFilter.tsx
+++ src/pages/admin/Dashboard/components/ApplicantListSection/ApplicationFilter.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { useApplicants } from '@/pages/admin/Dashboard/hooks/useApplicants';
 import { ApplicantFilterButton } from './ApplicationFilterButton';
 import type { ApplicationFilterOption } from '@/pages/admin/Dashboard/types/dashboard';
 
@@ -8,29 +9,31 @@ export type Props = {
 };
 
 export const ApplicationStatusFilter = ({ option, onOptionChange }: Props) => {
+  const { counts } = useApplicants(1);
+
   return (
     <Wrapper>
       <ApplicantFilterButton
         value={'ALL'}
-        label={'전체'}
+        label={`전체 (${counts.ALL})`}
         selected={option === 'ALL'}
         onClick={onOptionChange}
       />
       <ApplicantFilterButton
         value={'ACCEPTED'}
-        label={'합격'}
+        label={`합격 (${counts.ACCEPTED})`}
         selected={option === 'ACCEPTED'}
         onClick={onOptionChange}
       />
       <ApplicantFilterButton
         value={'REJECTED'}
-        label={'불합격'}
+        label={`불합격 (${counts.REJECTED})`}
         selected={option === 'REJECTED'}
         onClick={onOptionChange}
       />
       <ApplicantFilterButton
         value={'PENDING'}
-        label={'심사중'}
+        label={`심사중 (${counts.PENDING})`}
         selected={option === 'PENDING'}
         onClick={onOptionChange}
       />
