1891f7d2643d3ca5c147ce32e76d0564daad0fcd
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-13 01:06:19 +0900

    refactor: 상대경로에서 절대경로로 수정

--- src/pages/user/Main/api/club.ts
+++ src/pages/user/Main/api/club.ts
@@ -6,6 +6,8 @@ export type ClubResponse = {
 };
 
 export async function getClubsByCategory(filter: ClubCategoryEng): Promise<ClubResponse> {
-  const response = await fetch(import.meta.env.VITE_API_BASE_URL + `/clubs?category=${filter}`);
+  const url = `${import.meta.env.VITE_API_BASE_URL}/clubs?category=${filter}`;
+  const response = await fetch(url);
+
   return await response.json();
 }
