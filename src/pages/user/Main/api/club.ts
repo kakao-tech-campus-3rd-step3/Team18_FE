e1cb40f1ba79371cf4c620a15d199d2846774605
aaaaaattt <choigur03@gamil.com>
2025-10-12 20:41:17 +0900

    chore: lint 공백 제거

--- src/pages/user/Main/api/club.ts
+++ src/pages/user/Main/api/club.ts
@@ -6,7 +6,6 @@ export type ClubResponse = {
 };
 
 export async function getClubsByCategory(filter: ClubCategoryEng): Promise<ClubResponse> {
-
   const response = await fetch(import.meta.env.VITE_API_BASE_URL + `/clubs?category=${filter}`);
   return await response.json();
 }
