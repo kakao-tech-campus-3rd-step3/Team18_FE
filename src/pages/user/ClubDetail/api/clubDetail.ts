44d6570c97cc8fdec9827e10eae6cf16e08ffb46
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-12 21:33:47 +0900

    refactor: proxy 도입으로 api 상대경로로 수정

--- src/pages/user/ClubDetail/api/clubDetail.ts
+++ src/pages/user/ClubDetail/api/clubDetail.ts
@@ -1,11 +1,11 @@
 import type { ClubDetail } from '../types/clubDetail';
 
-export const fetchClubDetail = async (clubId: string | number): Promise<ClubDetail> => {
+export const fetchClubDetail = async (clubId: number): Promise<ClubDetail> => {
   const url = `/api/clubs/${clubId}`;
-  const response = await fetch(url);
+  const res = await fetch(url);
 
-  if (!response.ok) {
+  if (!res.ok) {
     throw new Error('동아리 상세 정보를 가져오는데 실패했습니다.');
   }
-  return response.json() as Promise<ClubDetail>;
+  return res.json() as Promise<ClubDetail>;
 };
