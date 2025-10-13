fec9b58caff470d1993b116c7d28012ad94bb35a
aaaaaattt <choigur03@gamil.com>
2025-10-13 16:31:44 +0900

    chore: Index 기반 배열 상태 업데이트 함수 util로 이동 - 특정 도메인에 종속X 판단

--- src/pages/user/Apply/domain/schedule.ts
+++ src/pages/user/Apply/domain/schedule.ts
@@ -24,17 +24,3 @@ export const updateSchedule = (
 
   return updatedSchedule;
 };
-
-export const updateSelectedState = (
-  selectedStates: boolean[],
-  startIndex: number,
-  selectedIndex: number,
-  selectionMode: boolean,
-): boolean[] => {
-  const start = Math.min(startIndex, selectedIndex);
-  const end = Math.max(startIndex, selectedIndex);
-
-  return selectedStates.map((isSelected, index) =>
-    index >= start && index <= end ? selectionMode : isSelected,
-  );
-};
