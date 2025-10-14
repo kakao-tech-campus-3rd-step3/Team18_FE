1d1ab187a2405e3a64a3567d7b015e98dc184db4
aaaaaattt <choigur03@gamil.com>
2025-10-14 16:41:19 +0900

    chore: import/order

--- src/pages/admin/Login/KakaoCallback.tsx
+++ src/pages/admin/Login/KakaoCallback.tsx
@@ -1,6 +1,6 @@
+import axios from 'axios';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import { LoadingSpinner } from '@/shared/components/LoadingSpinner';
 
 export const KakaoCallback = () => {
