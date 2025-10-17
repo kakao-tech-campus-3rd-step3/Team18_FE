00059acf2277d1087727b1dd7edc2fe93c37e9f2
aaaaaattt <choigur03@gamil.com>
2025-10-17 10:04:13 +0900

    refactor:  Callback 페이지의 API Layer 분리

---
+++ src/pages/admin/Login/api/postAuthCode.ts
@@ -0,0 +1,22 @@
+import type { AxiosResponse } from 'axios';
+import { apiInstance } from './initInstance';
+
+interface LoginSuccessResponse {
+  status: 'LOGIN_SUCCESS';
+  accessToken: string;
+  refreshToken: string;
+}
+
+interface RegistrationRequiredResponse {
+  status: 'REGISTRATION_REQUIRED';
+  temporaryToken: string;
+}
+
+type LoginResponse = LoginSuccessResponse | RegistrationRequiredResponse;
+
+export const postAuthCode = async (code: string): Promise<LoginResponse> => {
+  const response: AxiosResponse<LoginResponse> = await apiInstance.post('/auth/kakao/login', {
+    authorizationCode: code,
+  });
+  return response.data;
+};
