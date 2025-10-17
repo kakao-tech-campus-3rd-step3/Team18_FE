cf70fabc45fe61af88b2a73dfb9927d44c9d6823
aaaaaattt <choigur03@gamil.com>
2025-10-17 23:26:45 +0900

    refactor: 대시보드 api 마이그레이션

--- src/pages/admin/Dashboard/api/dashboard.ts
+++ src/pages/admin/Dashboard/api/dashboard.ts
@@ -1,12 +1,16 @@
+import { error } from 'console';
+import { isAxiosError } from 'axios';
+import { apiInstance } from '@/api/initInstance';
 import type { DashboardSummary } from '@/pages/admin/Dashboard/types/dashboard';
 
 export const fetchDashboardSummary = async (clubId: number): Promise<DashboardSummary> => {
-  const url = `${import.meta.env.VITE_API_BASE_URL}/clubs/${clubId}/dashboard`;
-  const response = await fetch(url);
-
-  if (!response.ok) {
-    throw new Error('대시보드 요약 정보를 불러오는 데 실패했습니다.');
+  try {
+    const { data } = await apiInstance.get(`/clubs/${clubId}/dashboard`);
+    return data;
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      throw new Error('대시보드 요약 정보를 불러오는 데 실패했습니다.');
+    }
   }
-
-  return response.json();
+  throw error;
 };
