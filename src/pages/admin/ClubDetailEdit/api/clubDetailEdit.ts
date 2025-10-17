fe00898cac6c412d78d93b55bbb9b5c642419b8b
aaaaaattt <choigur03@gamil.com>
2025-10-17 23:11:30 +0900

    refactor : 동아리 상세 수정 api 마이그레이션

--- src/pages/admin/ClubDetailEdit/api/clubDetailEdit.ts
+++ src/pages/admin/ClubDetailEdit/api/clubDetailEdit.ts
@@ -1,31 +1,23 @@
+import { apiInstance } from '@/api/initInstance';
 import type { ClubDetailEdit } from '../types/clubDetailEdit';
 
 export const fetchClubDetailEdit = async (clubId: string | number): Promise<ClubDetailEdit> => {
-  const url = `${import.meta.env.VITE_API_BASE_URL}/clubs/${clubId}`;
-  const response = await fetch(url);
-
-  if (!response.ok) {
+  try {
+    const { data } = await apiInstance.get(`/clubs/${clubId}`);
+    return data;
+  } catch {
     throw new Error('동아리 상세 수정 데이터를 가져오는데 실패했습니다.');
   }
-  return response.json() as Promise<ClubDetailEdit>;
 };
 
 export const updateClubDetailEdit = async (
   clubId: string | number,
   updatedData: Partial<ClubDetailEdit>,
 ): Promise<ClubDetailEdit> => {
-  const url = `${import.meta.env.VITE_API_BASE_URL}/clubs/${clubId}`;
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updatedData),
-  });
-
-  if (!response.ok) {
+  try {
+    const { data } = await apiInstance.post(`/clubs/${clubId}`, updatedData);
+    return data;
+  } catch {
     throw new Error('동아리 상세 정보를 수정하는데 실패했습니다.');
   }
-
-  return response.json() as Promise<ClubDetailEdit>;
 };
