c865a8ba23b9776fa647c21bbc7bab981e3b031a
aaaaaattt <choigur03@gamil.com>
2025-10-23 01:12:46 +0900

    refactor: ToastProvider 위치 변경 : main.tsx -> app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
+import { Toaster } from 'sonner';
 import { Navigation } from '@/shared/components/Navigation';
 import { theme } from '@/styles/theme';
 
@@ -10,9 +11,26 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <ToastProvider />
         <Navigation role='president' />
         <Outlet />
       </ThemeProvider>
     </QueryClientProvider>
   );
 }
+
+const ToastProvider = () => (
+  <Toaster
+    position='top-center'
+    richColors
+    toastOptions={{
+      style: {
+        borderRadius: theme.radius.md,
+        padding: '12px 20px',
+        fontWeight: theme.font.weight.bold,
+        fontSize: theme.font.size.base,
+        boxShadow: theme.shadow.md,
+      },
+    }}
+  />
+);
