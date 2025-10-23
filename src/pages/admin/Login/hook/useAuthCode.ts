90a504aef15a80d3269f9b92621f41f41a4cfab9
aaaaaattt <choigur03@gamil.com>
2025-10-24 08:26:45 +0900

    chore: 의존성 배열에 setup func에 사용된 util 함수 추가

--- src/pages/admin/Login/hook/useAuthCode.ts
+++ src/pages/admin/Login/hook/useAuthCode.ts
@@ -53,7 +53,7 @@ export const useAuthCode = (navigate: NavigateFunction) => {
       }
     };
     fetchToken();
-  }, []);
+  }, [navigate, setAccessToken, setTemporaryToken]);
 
   return { errorMessage, isLoading };
 };
