8ed0369558a036ccb3fb18fa0c0ce535714e6801
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-25 00:30:08 +0900

    style: 체크박스 옵션 지원폼 구현

---
+++ src/pages/admin/ApplicationFormBuilder/components/FieldsFormTable/FormFieldItem/Builders/CheckboxOptionsBuilder.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import styled from '@emotion/styled';
+import { OutlineInputField } from '@/shared/components/Form/InputField/OutlineInputField';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { FiPlus } from 'react-icons/fi';
+import { Text } from '@/shared/components/Text';
+import { IoCheckboxOutline } from 'react-icons/io5';
+
+export const CheckboxOptionsBuilder = () => {
+  const [options, setOptions] = useState('옵션');
+
+  return (
+    <Layput>
+      <OptionFieldWrapper>
+        <CheckboxIcon />
+        <OutlineInputField
+          value={options}
+          placeholder='옵션'
+          onChange={(e) => setOptions(e.target.value)}
+        />
+        <AiOutlineCloseCircle size={'1.5rem'} color='#757575' />
+      </OptionFieldWrapper>
+      <AddOptionButton>
+        <FiPlus /> <Text size='sm'>옵션 추가</Text>
+      </AddOptionButton>
+    </Layput>
+  );
+};
+
+const Layput = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+});
+
+const OptionFieldWrapper = styled.div({
+  display: 'flex',
+  gap: '1rem',
+  alignItems: 'center',
+});
+
+const CheckboxIcon = styled(IoCheckboxOutline)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.gray400};
+`;
+
+const AddOptionButton = styled.div(({ theme }) => ({
+  backgroundColor: theme.colors.gray100,
+  padding: '0.4rem',
+  width: '5.8rem',
+  display: 'flex',
+  gap: '1rem',
+  alignItems: 'center',
+  cursor: 'pointer',
+}));
