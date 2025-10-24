3c37946d8fa260a561f27f988b6348ed30b33052
aaaaaattt <choigur03@gamil.com>
2025-10-25 02:05:42 +0900

    feat : accessToken 만료 시 재발급 로직 추가 - axios interceptor 활용

--- src/api/initInstance.ts
+++ src/api/initInstance.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
-import { getAccessToken } from '@/pages/admin/Signup/utils/token';
-import type { AxiosInstance, CreateAxiosDefaults } from 'axios';
+import { getAccessToken, setAccessToken } from '@/pages/admin/Signup/utils/token';
+import { reissueAccessToken } from './auth';
+import type { ErrorResponse } from '@/pages/admin/Signup/type/error';
+import type { AxiosError, AxiosInstance, CreateAxiosDefaults } from 'axios';
 
 const initInstance = (config: CreateAxiosDefaults): AxiosInstance => {
   const instance = axios.create({
@@ -26,3 +28,24 @@ apiInstance.interceptors.request.use((config) => {
   }
   return config;
 });
+
+apiInstance.interceptors.response.use(
+  function onFulfilled(response) {
+    return response;
+  },
+  async function onRejected(e: AxiosError) {
+    const error = e as AxiosError<ErrorResponse>;
+    const config = error.config;
+    if (error.response?.data.status === 401 && config) {
+      try {
+        const tokenResponse = await reissueAccessToken();
+        setAccessToken(tokenResponse.accessToken);
+        config.headers.Authorization = `Bearer ${tokenResponse.accessToken}`;
+        return axios(config);
+      } catch {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  },
+);
