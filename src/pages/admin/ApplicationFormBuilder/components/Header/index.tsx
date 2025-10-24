64b7da5e9aba491f85b48db3ac8e8f9cfcedf3be
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-24 12:20:27 +0900

    style: 동아리 지원서 헤더 추가

---
+++ src/pages/admin/ApplicationFormBuilder/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+import { Button } from '@/shared/components/Button';
+import { Text } from '@/shared/components/Text';
+
+export const ApplicationFormBuilderHeader = () => {
+  return (
+    <Container>
+      <HeaderWrapper>
+        <Text size='xl' weight='medium'>
+          지원서 생성
+        </Text>
+        <Button variant='outline' width='5rem'>
+          수정하기
+        </Button>
+      </HeaderWrapper>
+      <Text size='sm' color='#339356'>
+        면접날짜는 타임 슬롯을 이용해서 안내할 수 있어요!
+      </Text>
+    </Container>
+  );
+};
+
+const Container = styled.div({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  padding: '2rem 0',
+  boxSizing: 'border-box',
+});
+
+const HeaderWrapper = styled.div({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
