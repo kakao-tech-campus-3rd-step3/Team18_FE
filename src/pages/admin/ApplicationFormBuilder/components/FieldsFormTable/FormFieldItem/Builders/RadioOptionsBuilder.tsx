075b6df3d8cfd0a09bf3a5a6e6307a658ed781ca
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-25 00:24:52 +0900

    style: 라디오 옵션  지원폼 구현

---
+++ src/pages/admin/ApplicationFormBuilder/components/FieldsFormTable/FormFieldItem/Builders/RadioOptionsBuilder.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import styled from '@emotion/styled';
+import { OutlineInputField } from '@/shared/components/Form/InputField/OutlineInputField';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { FiPlus } from 'react-icons/fi';
+import { Text } from '@/shared/components/Text';
+import { IoRadioButtonOffOutline } from 'react-icons/io5';
+
+export const RadioOptionsBuilder = () => {
+  const [radioInput, setRadioInput] = useState('옵션');
+
+  return (
+    <Layput>
+      <RadioOptionFieldWrapper>
+        <RadioIcon />
+        <OutlineInputField value={radioInput} placeholder='옵션' />
+        <AiOutlineCloseCircle size={'1.5rem'} color='#757575' />
+      </RadioOptionFieldWrapper>
+      <AddOptionButton>
+        <FiPlus /> <Text size='sm'>옵션 추가</Text>
+      </AddOptionButton>
+    </Layput>
+  );
+};
+
+const Layput = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+});
+
+const RadioOptionFieldWrapper = styled.div({
+  display: 'flex',
+  gap: '1rem',
+  alignItems: 'center',
+});
+
+const RadioIcon = styled(IoRadioButtonOffOutline)`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.gray400};
+`;
+
+const AddOptionButton = styled.div(({ theme }) => ({
+  backgroundColor: theme.colors.gray100,
+  padding: '0.4rem',
+  width: '5.8rem',
+  display: 'flex',
+  gap: '1rem',
+  alignItems: 'center',
+  cursor: 'pointer',
+}));
