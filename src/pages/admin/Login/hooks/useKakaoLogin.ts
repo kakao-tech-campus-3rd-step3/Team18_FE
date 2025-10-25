7e53f3395c58ef0602cb9b9f402c1696cb3fec7f
aaaaaattt <choigur03@gamil.com>
2025-10-25 23:20:23 +0900

    feat: login 로직 hook을 통한 관심사 분리

---
+++ src/pages/admin/Login/hooks/useKakaoLogin.ts
@@ -0,0 +1,40 @@
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+import { AuthContext } from '@/providers/auth';
+
+export function useKakaoLogin() {
+  const { login } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setIsLoading(true);
+    const controller = new AbortController();
+    const code = new URL(window.location.href).searchParams.get('code');
+
+    if (!code) {
+      navigate('/login');
+      return;
+    }
+
+    const processLogin = async () => {
+      try {
+        await login(code, controller.signal);
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          toast.error(e.message);
+        } else {
+          toast.error('로그인 중 오류가 발생했습니다.');
+        }
+      } finally {
+        setIsLoading(true);
+      }
+    };
+
+    processLogin();
+
+    return () => controller.abort();
+  }, [login, navigate]);
+  return { isLoading };
+}
