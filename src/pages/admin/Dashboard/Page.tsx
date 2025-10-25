724a6f6c6d07946de384837c7d7f3c6e629ddc04
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-25 23:50:43 +0900

    style: 지원자 관리 페이지 전체 넓이 수정

--- src/pages/admin/Dashboard/Page.tsx
+++ src/pages/admin/Dashboard/Page.tsx
@@ -5,14 +5,24 @@ import { DashboardSummarySection } from './components/DashboardSummarySection';
 export const DashboardPage = () => {
   return (
     <Layout>
-      <DashboardSummarySection />
-      <ApplicantListSection />
+      <Container>
+        <DashboardSummarySection />
+        <ApplicantListSection />
+      </Container>
     </Layout>
   );
 };
 
-const Layout = styled.main(({ theme }) => ({
-  backgroundColor: theme.colors.bgBlue,
+const Layout = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
   minHeight: '100vh',
-  padding: '2.6rem 6rem',
-}));
+  width: '100%',
+});
+
+const Container = styled.main({
+  padding: '2.5rem 0',
+  maxWidth: '1200px',
+  width: '100%',
+});
