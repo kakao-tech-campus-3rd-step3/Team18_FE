54a19fa1056fccfe7f3fcaa610f179385552934e
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-26 01:27:31 +0900

    style: 결과 전송하기 컴포넌트 구현

---
+++ src/pages/admin/Dashboard/components/SendResultButton/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/shared/components/Button';
+import styled from '@emotion/styled';
+
+export const SendResultButton = () => {
+  return (
+    <ButtonWrapper>
+      <Button width={'15rem'}>결과 전송하기</Button>
+    </ButtonWrapper>
+  );
+};
+
+const ButtonWrapper = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: '3rem',
+  width: '100%',
+});
