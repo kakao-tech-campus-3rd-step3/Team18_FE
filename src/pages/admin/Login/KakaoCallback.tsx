27228de7e07af110f1eadec649479faa54dc5869
aaaaaattt <choigur03@gamil.com>
2025-10-26 15:08:10 +0900

    style: LoadingSpinner 가운데 표시되도록 수정

--- src/pages/admin/Login/KakaoCallback.tsx
+++ src/pages/admin/Login/KakaoCallback.tsx
@@ -1,8 +1,23 @@
+import styled from '@emotion/styled';
 import { LoadingSpinner } from '@/shared/components/LoadingSpinner';
 import { useKakaoLogin } from './hooks/useKakaoLogin';
 
 export const KakaoCallback = () => {
   const { isLoading } = useKakaoLogin();
-  if (isLoading) return <LoadingSpinner />;
+  if (isLoading) return;
+  <LoadingSpinnerWrapper>
+    <LoadingSpinner />;
+  </LoadingSpinnerWrapper>;
   return null;
 };
+
+const LoadingSpinnerWrapper = styled.div({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
