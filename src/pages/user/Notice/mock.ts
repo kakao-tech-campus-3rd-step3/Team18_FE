c14f1d23ac7e5f27444a9e2637efc5b8ba082ca0
Ajin <ajin@Ajinui-noteubug.local>
2025-10-26 21:52:36 +0900

    chore: 서비스이메일 적용

--- src/pages/user/Notice/mock.ts
+++ src/pages/user/Notice/mock.ts
@@ -2,11 +2,11 @@
 
 export const mockNotices = {
   notices: [
-    { id: 1, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00' },
-    { id: 2, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00' },
-    { id: 3, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00' },
-    { id: 4, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00' },
-    { id: 5, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00' },
+    { id: 1, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00', author: '개발진' },
+    { id: 2, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00', author: '개발진' },
+    { id: 3, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00', author: '개발진' },
+    { id: 4, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00', author: '개발진' },
+    { id: 5, title: '동아리 평가 기준 안내', createdAt: '2024-05-23T00:00:00', author: '김라이언' },
   ],
 };
 
@@ -16,7 +16,7 @@ export const mockNoticeDetail = {
   content:
     '개편된 동아리 평가 기준 안내드립니다.\n동아리 평가 기준은 24-2학기부터 적용되니 참고 부탁드립니다.\n감사합니다.',
   createdAt: '2024-05-23T00:00:00',
-  author: '김라미언',
-  phone: '010-1010-1010',
+  author: '김라이언',
+  email: 'jnupole004@gmail.com',
   file: 'test.pdf',
 };
