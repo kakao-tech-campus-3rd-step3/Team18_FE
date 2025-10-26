44636e9bb67e140b38ef7f77ddb45740deea2aa3
aaaaaattt <choigur03@gamil.com>
2025-10-26 21:24:46 +0900

    feat : 동아리 필터링 dropdown 방식으로 변경 - placeholder 상태 관리 추가 - BannerSection props 추가(필터 상태변경 핸들러)

--- src/pages/user/Main/components/BannerSection/index.tsx
+++ src/pages/user/Main/components/BannerSection/index.tsx
@@ -1,23 +1,52 @@
 import styled from '@emotion/styled';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { ClubSearchInput } from '@/pages/user/Main/components/BannerSection/ClubSearchInput.tsx';
-import { CLUB_CATEGORY } from '@/pages/user/Main/constant/clubCategory.ts';
-import { RECRUIT_STATUS } from '@/pages/user/Main/types/club.ts';
+import {
+  CLUB_CATEGORY,
+  type ClubCategory,
+  type ClubCategoryEng,
+} from '@/pages/user/Main/constant/clubCategory.ts';
+import { RECRUIT_STATUS, type RecruitStatus } from '@/pages/user/Main/types/club.ts';
 import { Dropdown } from '@/shared/components/Dropdown/index.tsx';
 import * as S from './Banner.styled.ts';
+import { engToKorCategory, korToEngCategory } from '../../utils/formatting.ts';
 
 type Props = {
-  onChange: (s: string) => void;
+  onChange: (searchText: string) => void;
+  onSelect: (category: ClubCategoryEng) => void;
 };
 
-export const BannerSection = ({ onChange }: Props) => {
+export const BannerSection = ({ onChange, onSelect }: Props) => {
+  const [category, setCategory] = useState<ClubCategoryEng>('ALL');
+  const [recruitState, setRecruitState] = useState<RecruitStatus>('모집중');
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleClick = (newCategory: ClubCategory) => {
+    const engCategory = korToEngCategory[newCategory];
+    onSelect(engCategory);
+    searchParams.set('category', engCategory);
+    setSearchParams(searchParams);
+    setCategory(engCategory);
+  };
+
   return (
     <S.BannerWrapper>
       <S.BannerText>{` 동아리움 (Dongari-um): 동아리 + 공간(-um), 동아리들을 위한 공간.`}</S.BannerText>
 
       <SearchContainer>
         <ClubSearchInput onChange={onChange} />
-        <Dropdown value='동아리 필터' options={CLUB_CATEGORY} onSelect={() => {}}></Dropdown>
-        <Dropdown value='전체 보기' options={[...RECRUIT_STATUS]} onSelect={() => {}}></Dropdown>
+        <Dropdown
+          value={engToKorCategory[category]}
+          options={CLUB_CATEGORY}
+          onSelect={handleClick}
+        ></Dropdown>
+        <Dropdown
+          value={recruitState}
+          options={[...RECRUIT_STATUS]}
+          onSelect={() => setRecruitState}
+        ></Dropdown>
       </SearchContainer>
     </S.BannerWrapper>
   );
