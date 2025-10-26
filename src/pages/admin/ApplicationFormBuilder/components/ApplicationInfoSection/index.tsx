c23212ba4f5cd8ce8060959e3d45f54ac241654c
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-27 03:21:28 +0900

    feat: 동아리 정보 섹션에 데이트 피커 추가

--- src/pages/admin/ApplicationFormBuilder/components/ApplicationInfoSection/index.tsx
+++ src/pages/admin/ApplicationFormBuilder/components/ApplicationInfoSection/index.tsx
@@ -1,15 +1,48 @@
 import styled from '@emotion/styled';
 import { UnderlineInputField } from '@/shared/components/Form/InputField/UnderlineInputField';
+import { useTimeslotState } from '@/pages/admin/ApplicationFormBuilder/hooks/useTimeslotState';
+import { ko } from 'date-fns/locale';
+import { datePickerStyles } from '@/pages/admin/ApplicationFormBuilder/styles/datepicker.styled';
+import * as S from '@/pages/admin/ApplicationFormBuilder/styles/timeslot.styled';
+import DatePicker from 'react-datepicker';
+import { Text } from '@/shared/components/Text';
+import { Global } from '@emotion/react';
+import type { CustomInputProps } from '@/pages/admin/ApplicationFormBuilder/types/clubInfo';
+
+const CustomInput = ({ value, onClick }: CustomInputProps) => (
+  <S.CustomInputWrapper onClick={onClick}>
+    <Text color='#757575' size='sm'>
+      {value || '날짜를 선택하세요'}
+    </Text>
+  </S.CustomInputWrapper>
+);
 
 export const ApplicationInfoSection = () => {
+  const { startDate, endDate, formatDateRange, handleDateChange } = useTimeslotState();
+
   return (
-    <Layout>
-      <Wrapper>
-        <UnderlineInputField placeholder='ex. 동아리명 10기 신입부원 모집' />
-        <UnderlineInputField placeholder='2025.10.1 - 2025.10.13' />
-      </Wrapper>
-      <UnderlineInputField placeholder='한줄 소개 작성해주세요.' />
-    </Layout>
+    <>
+      <Global styles={datePickerStyles} />
+      <Layout>
+        <Wrapper>
+          <UnderlineInputField placeholder='ex. 동아리명 10기 신입부원 모집' />
+          <S.DatePickerWrapper>
+            <DatePicker
+              locale={ko}
+              dateFormat='yyyy.MM.dd'
+              selected={startDate}
+              onChange={handleDateChange}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+              customInput={<CustomInput value={formatDateRange()} />}
+              popperPlacement='bottom'
+            />
+          </S.DatePickerWrapper>
+        </Wrapper>
+        <UnderlineInputField placeholder='한줄 소개 작성해주세요.' />
+      </Layout>
+    </>
   );
 };
 
