9949bf1e070eecdd3f87427bf86dd7551d338386
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-27 19:01:58 +0900

    feat: 항목 추가 버튼 클릭 시 새로운 fieldsForm 생성

--- src/pages/admin/ApplicationFormBuilder/components/FieldsFormTableSection/index.tsx
+++ src/pages/admin/ApplicationFormBuilder/components/FieldsFormTableSection/index.tsx
@@ -1,11 +1,49 @@
+import styled from '@emotion/styled';
+
 import { AddFieldButton } from './AddFieldButton';
 import { FormFieldItem } from './FormFieldItem';
+import type { ApplicationForm } from '@/pages/admin/ApplicationFormBuilder/types/fieldType';
+import type { UseFormReturn } from 'react-hook-form';
+import { useFieldArray } from 'react-hook-form';
 
-export const ApplicationFieldsFormTableSection = () => {
+type Props = {
+  formHandler: UseFormReturn<ApplicationForm>;
+};
+
+export const ApplicationFieldsFormTableSection = ({ formHandler }: Props) => {
+  const { control } = formHandler;
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'questions',
+  });
+
+  const handleAddFormField = () => {
+    append({
+      questionNum: fields.length + 1,
+      questionType: 'TEXT',
+      question: '',
+      required: true,
+      optionList: [],
+      timeSlotOptions: [],
+    });
+  };
+
   return (
-    <div>
-      <AddFieldButton />
-      <FormFieldItem />
-    </div>
+    <>
+      {fields.map((data, index) => (
+        <div key={data.id}>
+          {index !== 0 && <Divider />}
+          <FormFieldItem index={index} formHandler={formHandler} onRemove={() => remove(index)} />
+        </div>
+      ))}
+      <AddFieldButton onClick={handleAddFormField} />
+    </>
   );
 };
+
+const Divider = styled.div(({ theme }) => ({
+  borderBottom: `1px solid ${theme.colors.gray200}`,
+  width: '100%',
+  margin: '2rem 0 1.3rem 0',
+}));
