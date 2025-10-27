27dca96f30ffdbd6273e42e4e99eda655a3507d9
kanghaeun <xgkg0330@jnu.ac.kr>
2025-10-27 21:32:35 +0900

    feat: 텍스트 placeholder 수정

--- src/pages/admin/ApplicationFormBuilder/components/FieldsFormTableSection/FormFieldItem/Builders/TextOptionsBuilder.tsx
+++ src/pages/admin/ApplicationFormBuilder/components/FieldsFormTableSection/FormFieldItem/Builders/TextOptionsBuilder.tsx
@@ -1,17 +1,10 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
 import { OutlineTextareaField } from '@/shared/components/Form/TextAreaField/OutlineTextareaField';
 
 export const TextOptionsBuilder = () => {
-  const [placeholder, setPlaceholder] = useState('');
-
   return (
     <Layout>
-      <OutlineTextareaField
-        placeholder='텍스트를 작성해주세요.'
-        value={placeholder}
-        onChange={(e) => setPlaceholder(e.target.value)}
-      />
+      <OutlineTextareaField placeholder='지원자가 작성할 텍스트 창입니다. 작성하지 마세요.' />
     </Layout>
   );
 };
