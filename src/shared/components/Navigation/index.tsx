e8c703a63e591a00facf17496ce0dff657a21ed6
aaaaaattt <choigur03@gamil.com>
2025-10-28 11:06:52 +0900

    style: 로그인/로그아웃과 기본 nav 구성 위치 고정 - LeftMenu(로그인, 로그아웃을 제외한 nav) - RightMenu(로그인, 로그아웃)

--- src/shared/components/Navigation/index.tsx
+++ src/shared/components/Navigation/index.tsx
@@ -11,6 +11,11 @@ export const Navigation = () => {
   const items = NAV_CONFIG[user?.role ?? 'guest'];
   const { logout } = useContext(AuthContext);
 
+  const items = NAV_CONFIG[user?.role ?? 'guest'];
+
+  const leftItems = items.filter((item) => !['login', 'logout'].includes(item.key));
+  const rightItem = items.find((item) => ['login', 'logout'].includes(item.key));
+
   const handleItemClick = (label: React.ReactNode) => {
     if (typeof label === 'string') {
       setCurrentRoute(label);
@@ -23,24 +28,58 @@ export const Navigation = () => {
 
   return (
     <NavigationContainer selectedItem={currentRoute}>
-      {items.map((item) => {
-        const path =
-          item.to?.includes(':clubId') && user?.clubId?.length
-            ? replaceRouteParams(item.to, { clubId: user.clubId[0] })
-            : item.to;
+      <LeftMenu>
+        {leftItems.map((item) => {
+          const path =
+            item.to?.includes(':clubId') && user?.clubId?.length
+              ? replaceRouteParams(item.to, { clubId: user.clubId[0] })
+              : item.to;
 
-        return (
+          return (
+            <NavigationItem
+              key={item.key}
+              to={path}
+              isLogo={item.isLogo}
+              selected={currentRoute === location.pathname}
+              onClick={() => {
+                if (item.label == '로그아웃') {
+                  logout();
+                } else {
+                  handleItemClick(item.label);
+                }
+              }}
+            >
+              {item.label}
+            </NavigationItem>
+          );
+        })}
+      </LeftMenu>
+
+      <RightMenu>
+        {rightItem && (
           <NavigationItem
-            key={item.key}
-            to={path}
-            isLogo={item.isLogo}
-            selected={currentRoute === item.label}
-            onClick={handleItemClick}
+            key={rightItem.key}
+            to={rightItem.to}
+            isLogo={rightItem.isLogo}
+            selected={location.pathname === rightItem.to}
+            onClick={() => {
+              if (rightItem.key === 'logout') logout();
+            }}
           >
-            {item.label}
+            {rightItem.label}
           </NavigationItem>
-        );
-      })}
+        )}
+      </RightMenu>
     </NavigationContainer>
   );
 };
+
+const LeftMenu = styled.div({
+  display: 'flex',
+  gap: '4rem',
+});
+
+const RightMenu = styled.div({
+  marginLeft: 'auto',
+  display: 'flex',
+});
