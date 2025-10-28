a1fe872c89827a44b4837f4e7ef6cda19ca89719
ganimjeong <ganimjeong@gmail.com>
2025-10-28 21:56:58 +0900

    feat: msw클라이언트에 공지사항 추가

--- src/mocks/client.ts
+++ src/mocks/client.ts
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw/browser';
 import { applicantHandlers } from '@/mocks/handler/applicant';
 import { clubHandlers } from '@/mocks/handler/club';
+import { noticeHandlers } from '@/mocks/handler/notice';
 
-export const client = setupWorker(...clubHandlers, ...applicantHandlers);
+export const client = setupWorker(...clubHandlers, ...applicantHandlers, ...noticeHandlers);
