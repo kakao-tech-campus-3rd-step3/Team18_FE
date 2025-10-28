da0c6f61a984afa6936083a43ad67e261e2f70a9
aaaaaattt <choigur03@gamil.com>
2025-10-28 13:35:25 +0900

    feat: default role 설정 추가 - role : guest

--- src/providers/auth.tsx
+++ src/providers/auth.tsx
@@ -17,7 +17,7 @@ export const AuthContext = createContext<AuthContextType>({
 });
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>({ role: 'guest' });
   const navigate = useNavigate();
 
   const login = async (code: string, signal: AbortSignal) => {
