5784a918a2ddf75a31890da92cafc303bdccca72
aaaaaattt <choigur03@gamil.com>
2025-10-29 15:22:30 +0900

    feat: 연락받을 이메일 상수 관리

--- src/shared/components/Footer/index.tsx
+++ src/shared/components/Footer/index.tsx
@@ -1,9 +1,10 @@
+import { CONTACT_EMAIL } from '@/shared/constants/email';
 import { Copyright, Email, FooterContainer } from './index.styled';
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <Email href='mailto:jnupole004@gmail.com'>Contact : jnupole004@gmail.com</Email>
+      <Email href='mailto:jnupole004@gmail.com'>Contact : {CONTACT_EMAIL}</Email>
       <Copyright>© {new Date().getFullYear()} Dongarium. All rights reserved.</Copyright>
     </FooterContainer>
   );
