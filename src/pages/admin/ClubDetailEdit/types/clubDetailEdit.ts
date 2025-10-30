96afb012834b8efd5a5891d4e766f862334aa862
ganimjeong <ganimjeong@gmail.com>
2025-10-30 21:19:11 +0900

    feat: api변경을 위한 타입분리

--- src/pages/admin/ClubDetailEdit/types/clubDetailEdit.ts
+++ src/pages/admin/ClubDetailEdit/types/clubDetailEdit.ts
@@ -1,3 +1,12 @@
 import type { ClubDetail } from '@/pages/user/ClubDetail/types/clubDetail';
 
+// GET용: 전체 데이터
 export type ClubDetailEdit = ClubDetail;
+
+// PATCH or POST용: 이미지 제외한 정보 수정
+export type ClubDetailUpdatePayload = Omit<ClubDetailEdit, 'introductionImages'>;
+
+// PUT용: 이미지 전용 수정
+export type ClubDetailImagesPayload = {
+  introductionImages: File[];
+};
