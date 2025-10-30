cbe1fb9ce4ec93fa08b0e4bb48edaa7bda8bfcb6
aaaaaattt <choigur03@gamil.com>
2025-10-30 09:52:22 +0900

    chore: import path 수정

--- src/providers/guards/ClubGuard.tsx
+++ src/providers/guards/ClubGuard.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import { AuthContext } from '@/providers/auth';
-import { EmptyState } from '@/shared/Pages/emptyState';
+import { EmptyState } from '@/shared/components/EmptyState';
 export const ClubGuard = () => {
   const { user } = useContext(AuthContext);
   if (!user?.clubId?.length) {
