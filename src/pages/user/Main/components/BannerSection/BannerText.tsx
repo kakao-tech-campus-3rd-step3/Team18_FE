b8be94600d1fcb1c0ba230b836fe7cbfda60e52c
aaaaaattt <choigur03@gamil.com>
2025-10-30 17:40:13 +0900

    style : bannerText 사이 간격 추가 & props 객체형태로 매개변수 전달

--- src/pages/user/Main/components/BannerSection/BannerText.tsx
+++ src/pages/user/Main/components/BannerSection/BannerText.tsx
@@ -5,13 +5,20 @@ import { theme } from '@/styles/theme';
 export const BannerTextWrapper = styled.div({
   textAlign: 'center',
   marginBottom: '24px',
-  gap: '8px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: '16px',
 });
 
-export const HeaderText = (children: { children: React.ReactNode }) => (
-  <Text size='xl' weight='bold' color={theme.colors.textPrimary} {...children} />
+export const HeaderText = ({ children }: { children: React.ReactNode }) => (
+  <Text size='xl' weight='bold' color={theme.colors.textPrimary}>
+    {children}
+  </Text>
 );
 
-export const SubText = (children: { children: React.ReactNode }) => (
-  <Text size='sm' weight='regular' color={theme.colors.textSecondary} {...children} />
+export const SubText = ({ children }: { children: React.ReactNode }) => (
+  <Text size='sm' weight='regular' color={theme.colors.textSecondary}>
+    {children}
+  </Text>
 );
