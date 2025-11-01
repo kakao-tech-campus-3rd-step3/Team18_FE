b8bd183b86d072ece2b2c2e48cbc335d46a1c675
aaaaaattt <choigur03@gamil.com>
2025-11-01 23:25:58 +0900

    refactor : refresh 만료 여부는 interceptor에서 처리하도록 수정

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,22 +1,10 @@
 import { apiInstance } from './initInstance';
-import type { ErrorResponse } from '@/pages/admin/Signup/type/error';
-import type { AxiosError } from 'axios';
 
 export interface TokenResponse {
   accessToken: string;
 }
 
-const EXPIRED_REFRESH_TOKEN: string = 'EXPIRED_REFRESH_TOKEN';
-
 export const reissueAccessToken = async (): Promise<TokenResponse> => {
-  try {
-    const { data } = await apiInstance.post(`/auth/reissue`, {}, { withCredentials: true });
-    return data;
-  } catch (e: unknown) {
-    const error = e as AxiosError<ErrorResponse>;
-    if (error.response?.data.error_code === EXPIRED_REFRESH_TOKEN) {
-      throw new Error('Refresh token 만료');
-    }
-    throw new Error(error.message);
-  }
+  const { data } = await apiInstance.post(`/auth/reissue`, {}, { withCredentials: true });
+  return data;
 };
