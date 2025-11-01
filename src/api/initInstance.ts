61d24bf8d2eb5192f607eb2e310c48668b748059
aaaaaattt <choigur03@gamil.com>
2025-11-01 19:00:18 +0900

    feat : refreshToken 에러 명세 반영

--- src/api/initInstance.ts
+++ src/api/initInstance.ts
@@ -5,6 +5,11 @@ import type { ErrorResponse } from '@/pages/admin/Signup/type/error';
 import type { AxiosError, AxiosInstance, CreateAxiosDefaults } from 'axios';
 
 const EXPIRED_ACCESS_TOKEN: string = 'EXPIRED_ACCESS_TOKEN';
+const INVALID_INPUT_VALUE: string = 'INVALID_INPUT_VALUE';
+const UNSUPPORTED_JWT: string = 'UNSUPPORTED_JWT';
+const UNAUTHENTICATED_USER: string = 'UNAUTHENTICATED_USER';
+const INVALID_JWT_SIGNATURE: string = 'INVALID_JWT_SIGNATURE';
+const EXPIRED_REFRESH_TOKEN: string = 'EXPIRED_REFRESH_TOKEN';
 
 const initInstance = (config: CreateAxiosDefaults): AxiosInstance => {
   const instance = axios.create({
@@ -44,8 +49,26 @@ apiInstance.interceptors.response.use(
         setAccessToken(tokenResponse.accessToken);
         config.headers.Authorization = `Bearer ${tokenResponse.accessToken}`;
         return axios(config);
-      } catch {
-        window.location.href = '/login';
+      } catch (e: unknown) {
+        if (axios.isAxiosError(e)) {
+          const error = e as AxiosError<ErrorResponse>;
+          const errorMessage = error.response?.data.message;
+          switch (error.response?.data.error_code) {
+            case INVALID_INPUT_VALUE:
+              throw new Error(`${errorMessage}`);
+            case UNAUTHENTICATED_USER:
+              throw new Error(`${errorMessage}`);
+            case UNSUPPORTED_JWT:
+              throw new Error(`${errorMessage}`);
+            case INVALID_JWT_SIGNATURE:
+              throw new Error(` ${errorMessage}`);
+            case EXPIRED_REFRESH_TOKEN:
+              throw new Error(`${errorMessage}`);
+            default:
+              throw new Error(`알 수 없는 오류: ${e.message}`);
+          }
+        }
+        throw e;
       }
     }
     return Promise.reject(error);
