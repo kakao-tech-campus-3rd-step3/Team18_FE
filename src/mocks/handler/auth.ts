e3ad665720d730296bbb32bc44de126f7fcdb19b
aaaaaattt <choigur03@gamil.com>
2025-11-03 14:31:19 +0900

    feat : 로컬 msw 테스트 환경 구축

---
+++ src/mocks/handler/auth.ts
@@ -0,0 +1,35 @@
+import { http, HttpResponse } from 'msw';
+import type { LoginResponse } from '@/pages/admin/Login/api/auth';
+
+interface AuthRequest {
+  authorizationCode: string;
+}
+export const authHandlers = [
+  http.post('/api/auth/kakao/login', async ({ request }) => {
+    const { authorizationCode } = (await request.json()) as AuthRequest;
+
+    if (authorizationCode === 'VALID_CODE') {
+      const response: LoginResponse = {
+        status: 'LOGIN_SUCCESS',
+        accessToken: 'mock-access-token',
+        clubIdAndRoleList: [],
+      };
+      return HttpResponse.json(response, { status: 200 });
+    }
+
+    if (authorizationCode === 'REGISTRATION_REQUIRED') {
+      const response: LoginResponse = {
+        status: 'REGISTRATION_REQUIRED',
+        temporaryToken: 'mock-temporary-token',
+        clubIdAndRoleList: [],
+      };
+      return HttpResponse.json(response, { status: 200 });
+    }
+
+    return HttpResponse.json({ message: '코드 에러' }, { status: 400 });
+  }),
+
+  http.post('/api/auth/logout', () => {
+    return HttpResponse.json({ message: '로그아웃 성공' }, { status: 200 });
+  }),
+];
