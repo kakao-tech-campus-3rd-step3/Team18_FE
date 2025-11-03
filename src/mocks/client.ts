4b0960c81000b121f400293ad5f3fdef019de02e
aaaaaattt <choigur03@gamil.com>
2025-11-03 14:31:19 +0900

    feat : 로컬 msw 테스트 환경 구축

--- src/mocks/client.ts
+++ src/mocks/client.ts
@@ -3,11 +3,12 @@ import { applicantHandlers } from '@/mocks/handler/applicant';
 import { clubHandlers } from '@/mocks/handler/club';
 import { noticeHandlers } from '@/mocks/handler/notice';
 import { applyFormHandlers } from './handler/applyForm';
+import { authHandlers } from './handler/auth';
 
 export const client = setupWorker(
   ...clubHandlers,
   ...applicantHandlers,
   ...noticeHandlers,
   ...applyFormHandlers,
+  ...authHandlers,
 );
-
