112b12c0c7dec487041e70cd3ed68b3e88dac511
kanghaeun <xgkg0330@jnu.ac.kr>
2025-11-03 21:06:49 +0900

    chore: client 린트 적용

--- src/mocks/client.ts
+++ src/mocks/client.ts
@@ -2,8 +2,8 @@ import { setupWorker } from 'msw/browser';
 import { applicantHandlers } from '@/mocks/handler/applicant';
 import { clubHandlers } from '@/mocks/handler/club';
 import { noticeHandlers } from '@/mocks/handler/notice';
-import { authHandlers } from './handler/auth';
 import { applyFormHandlers } from './handler/applyForm';
+import { authHandlers } from './handler/auth';
 
 export const client = setupWorker(
   ...clubHandlers,
