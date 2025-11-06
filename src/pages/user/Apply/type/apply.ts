99d51435f1a9c80fbf09cdd6a9bd0a31dbd6f6ab
aaaaaattt <choigur03@gamil.com>
2025-11-06 13:57:08 +0900

    refactor : RHF의 answers 필드 타입 추가(Answer : {value : ..  , questionType : ..}

--- src/pages/user/Apply/type/apply.ts
+++ src/pages/user/Apply/type/apply.ts
@@ -71,8 +71,13 @@ export type FormInputs = {
   department: string;
   phoneNumber: string;
   email: string;
-  answers: object[];
   selectedInterviewSchedule: PostInterviewSchedule[];
+  answers: Answer[];
+};
+
+export type Answer = {
+  value: string | SelectedInterviewValue | null;
+  questionType: (typeof QuestionTypes)[keyof typeof QuestionTypes];
 };
 
 export type DragState = {
