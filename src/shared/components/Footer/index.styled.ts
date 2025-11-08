120f293ede9fc5912c10d1dc83d2df3066ff5eb1
aaaaaattt <choigur03@gamil.com>
2025-11-09 02:33:29 +0900

    style : footer가 페이지 영역을 가리지 않도록 수정

--- src/shared/components/Footer/index.styled.ts
+++ src/shared/components/Footer/index.styled.ts
@@ -1,10 +1,6 @@
 import styled from '@emotion/styled';
 
 export const FooterContainer = styled.div(({ theme }) => ({
-  position: 'fixed',
-  bottom: 0,
-  left: 0,
-  width: '100%',
   backgroundColor: theme.colors.gray100,
   padding: 32,
   borderTop: `1px solid ${theme.colors.primary00}`,
