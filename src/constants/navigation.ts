7b9e021adf105a0d7f526c09b37325f6fe3e468f
aaaaaattt <choigur03@gamil.com>
2025-11-09 00:06:51 +0900

    fix : 네비게이션 텍스트 수정

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -3,7 +3,7 @@ import { type Role, type NavItemData, ROLE } from '@/types/navigation';
 export const getNavItems = (role: Role): NavItemData[] => {
   const commonItems: NavItemData[] = [
     { key: 'logo', label: '동아리움', to: '/', isLogo: true },
-    { key: 'notices', label: '공지 사항', to: '/notices' },
+    { key: 'notices', label: '공지사항', to: '/notices' },
   ];
 
   if (role === ROLE.CLUB_MEMBER || role === ROLE.APPLICANT) {
@@ -13,9 +13,9 @@ export const getNavItems = (role: Role): NavItemData[] => {
   if (role === ROLE.CLUB_ADMIN || role === ROLE.CLUB_EXECUTIVE) {
     return [
       ...commonItems,
-      { key: 'applicants', label: '지원자관리', to: '/admin/clubs/:clubId/dashboard' },
-      { key: 'clubEdit', label: '동아리페이지관리', to: '/admin/clubs/:clubId/edit' },
-      { key: 'form', label: '지원폼관리', to: '/admin/clubs/:clubId/application/form/create' },
+      { key: 'applicants', label: '지원자 관리', to: '/admin/clubs/:clubId/dashboard' },
+      { key: 'clubEdit', label: '동아리페이지 관리', to: '/admin/clubs/:clubId/edit' },
+      { key: 'form', label: '지원폼 관리', to: '/admin/clubs/:clubId/application/form/create' },
       { key: 'logout', label: '로그아웃', to: '#' },
     ];
   }
