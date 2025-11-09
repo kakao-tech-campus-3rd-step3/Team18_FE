64eff7e039918cdc364d20e13263f74654191e0b
kanghaeun <xgkg0330@jnu.ac.kr>
2025-11-10 00:02:08 +0900

    style: 클럽 카드 호버 스타일 추가

--- src/pages/user/Main/components/ClubListSection/Club.styled.ts
+++ src/pages/user/Main/components/ClubListSection/Club.styled.ts
@@ -1,12 +1,6 @@
 import styled from '@emotion/styled';
 import type { RecruitStatus } from '@/pages/user/Main/types/club';
 
-export const ClubCategoryText = styled.div(({ theme }) => ({
-  fontSize: theme?.font?.size?.xs,
-  fontWeight: theme.font.weight.regular,
-  color: theme?.colors.textSecondary,
-}));
-
 export const ClubNameText = styled.div(({ theme }) => ({
   fontSize: theme?.font?.size?.lg,
   fontWeight: theme.font.weight.bold,
@@ -32,6 +26,12 @@ type Props = {
   status: RecruitStatus;
 };
 
+export const StatusContainer = styled.div({
+  display: 'flex',
+  gap: '8px',
+  alignItems: 'center',
+});
+
 export const RecruitStatusBox = styled.div<Props>(({ theme, status }) => ({
   display: 'flex',
   alignItems: 'center',
@@ -43,11 +43,29 @@ export const RecruitStatusBox = styled.div<Props>(({ theme, status }) => ({
   backgroundColor: status === '모집중' ? theme.colors.primary : theme.colors.gray200,
 }));
 
-export const RecruitStatusText = styled.div<Props>(({ theme }) => ({
+export const RecruitStatusText = styled.div<Props>(({ theme, status }) => ({
   textAlign: `center`,
   fontSize: theme.font.size.xs,
   fontWeight: theme.font.weight.medium,
-  color: 'white',
+  color: status === '모집중' ? theme.colors.primary00 : theme.colors.textPrimary,
+  lineHeight: 1.2,
+}));
+
+export const CategoryStatusBox = styled.div(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center',
+  padding: '6px 10px',
+  borderRadius: '10rem',
+  backgroundColor: theme.colors.gray200,
+}));
+
+export const CategoryStatusText = styled.div(({ theme }) => ({
+  textAlign: `center`,
+  fontSize: theme.font.size.xs,
+  fontWeight: theme.font.weight.medium,
+  color: theme.colors.textPrimary,
   lineHeight: 1.2,
 }));
 
@@ -82,8 +100,13 @@ export const ClubItem = styled.div(({ theme }) => ({
   marginBottom: 10,
   border: `1px solid #E6E8EB`,
   borderRadius: theme.radius.md,
-  padding: 16,
+  padding: '1.7rem 1.5rem',
   gap: 16,
+  transition: 'transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out',
+  '&:hover': {
+    transform: 'scale(1.02)',
+    boxShadow: `0 0 10px ${theme.colors.blue200}50`,
+  },
 }));
 
 export const NoSearchResultContainer = styled.div({
