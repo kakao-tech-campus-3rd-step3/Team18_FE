57c985d4edcd10f84b06ed4dc0136df5e59aea37
ganimjeong <ganimjeong@gmail.com>
2025-11-10 21:11:42 +0900

    refactor: 인증 관련 User, ContextType을 별도 타입 파일로 분리

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,10 +1,13 @@
 import type { Role } from './navigation';
-import type { LoginResponse } from '@/pages/admin/Login/api/auth';
+import type { LoginResponse, ClubMemberInfo } from '@/pages/admin/Login/api/auth';
+import type { SignupFormInputs } from '@/pages/admin/Signup/type/signup';
 
 export type User = {
   role: Role;
   clubId?: number;
   clubName?: string;
+  userId?: number;
+  clubAndRoleList?: ClubMemberInfo[];
 };
 
 export type AuthContextType = {
@@ -12,5 +15,5 @@ export type AuthContextType = {
   setUser: (user: User) => void;
   login: (code: string, signal: AbortSignal) => Promise<LoginResponse>;
   logout: () => Promise<void>;
-  completeSignup: (accessToken: string) => void;
+  completeSignup: (signupFormValue: SignupFormInputs, temporaryToken: string) => void;
 };
