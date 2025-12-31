bad14061de80d39802a1e1e136a6d8329e5d1674
kanghaeun <xgkg0330@jnu.ac.kr>
2026-01-01 03:33:49 +0900

    feat: 구글 애널리틱스(GA4) 연동 및 페이지 추적 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import { UserProvider } from '@/app/providers/auth';
 import { theme } from '@/app/styles/theme';
@@ -9,9 +11,29 @@ import { ScrollToTop } from '@/shared/components/ScrollToTop';
 import { AppContainer, MainContent } from './app/styles/Layout/App.styled';
 import Footer from './shared/components/Footer';
 
+declare global {
+  interface Window {
+    GA_INITIALIZED: boolean;
+  }
+}
+
 const queryClient = new QueryClient();
 
+const GA_TRACKING_ID = 'G-WVZCKKQ8HQ';
+
 export function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!window.window.GA_INITIALIZED) {
+      ReactGA.initialize(GA_TRACKING_ID);
+      window.window.GA_INITIALIZED = true;
+    }
+
+    ReactGA.set({ page: location.pathname });
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search });
+  }, [location]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
