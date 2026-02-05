0987fd7bb54f6d8ead1e9c64db4b18974fb9a0da
ganimjeong <ganimjeong@gmail.com>
2026-02-05 22:21:47 +0900

    feat: MEMBER_MANAGEMENT 상수 정의

--- src/app/constants/routerPath.ts
+++ src/app/constants/routerPath.ts
@@ -8,6 +8,7 @@ export const ROUTE_PATH = {
     APPLICATION_DETAIL: 'clubs/:clubId/applicants/:applicantId',
     CLUB_EDIT: 'clubs/:clubId/edit',
     APPLICATION_FORM_BUILDER: 'clubs/:clubId/application/form/create',
+    MEMBER_MANAGEMENT: '/admin/clubs/:clubId/members',
   },
 
   COMMON: {
