9999de029d9829c2a9da01d1b6cc2d019444903a
kanghaeun <xgkg0330@jnu.ac.kr>
2026-02-24 00:17:07 +0900

    feat: 특정 날짜에 따른 시간 선택 리스트 컴포넌트 생성

---
+++ src/pages/user/Apply/components/ApplicationForm/HorizontalInterviewScheduleSelector.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { Button } from '@/shared/components/Button';
+import { DatePickerRow } from './index.styled';
+import { InterviewScheduleSelector } from './InterviewScheduleSelector';
+import type { InterviewSchedule, SelectedInterviewValue } from '@/pages/user/Apply/type/apply';
+
+type Props = {
+  timeSlotOptions: InterviewSchedule[];
+  onChange?: (value: SelectedInterviewValue) => void;
+};
+
+export const HorizontalInterviewScheduleSelector = ({ timeSlotOptions, onChange }: Props) => {
+  const [selectedDateIdx, setSelectedDateIdx] = useState(0);
+
+  return (
+    <div>
+      <DatePickerRow>
+        {timeSlotOptions.map((schedule, idx) => (
+          <Button
+            key={idx}
+            type='button'
+            variant={selectedDateIdx === idx ? undefined : 'outline'}
+            onClick={() => setSelectedDateIdx(idx)}
+            width='8rem'
+          >
+            {schedule.date}
+          </Button>
+        ))}
+      </DatePickerRow>
+
+      {timeSlotOptions.map((schedule, idx) => (
+        <div key={idx} style={{ display: selectedDateIdx === idx ? 'block' : 'none' }}>
+          <InterviewScheduleSelector
+            date={schedule.date}
+            availableTime={schedule.availableTime}
+            onChange={onChange}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
