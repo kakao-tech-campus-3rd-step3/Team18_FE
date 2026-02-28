fbbee0b3ff04322fb9ad731c6263dd1a6ddf8aed
ganimjeong <ganimjeong@gmail.com>
2026-02-28 17:49:58 +0900

    fix: cicd에러 해결 : 클럽가드 이전 코드로 복귀

--- src/app/providers/guards/ClubGuard.tsx
+++ src/app/providers/guards/ClubGuard.tsx
@@ -1,14 +1,8 @@
 import { Outlet } from 'react-router-dom';
 import { EmptyState } from '@/shared/components/EmptyState';
 import { useAuth } from '../auth';
-
 export const ClubGuard = () => {
   const { user } = useAuth();
-
-  if (import.meta.env.DEV) {
-    return <Outlet />;
-  }
-
   if (!user?.clubId) {
     return <EmptyState />;
   }
