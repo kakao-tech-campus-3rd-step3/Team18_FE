5012fff6f08e03c804d9b42fee080a4a646997a1
ganimjeong <ganimjeong@gmail.com>
2026-04-20 20:47:45 +0900

    fix: 임시로 회장전화번호란 주석처리

--- src/pages/user/ClubDetail/components/ClubInfoSidebarSection/index.tsx
+++ src/pages/user/ClubDetail/components/ClubInfoSidebarSection/index.tsx
@@ -13,7 +13,7 @@ export const ClubInfoSidebarSection = ({
   clubId,
   clubName,
   presidentName,
-  presidentPhoneNumber,
+  // presidentPhoneNumber,
   location,
   recruitStart,
   recruitEnd,
@@ -29,9 +29,9 @@ export const ClubInfoSidebarSection = ({
   return (
     <SidebarContainer>
       <ClubInfoRow icon={<BiListUl />} label='회장 이름' value={presidentName} />
-      {!/^010-0000/.test(presidentPhoneNumber) && (
+      {/* {!/^010-0000/.test(presidentPhoneNumber) && (
         <ClubInfoRow icon={<BiListUl />} label='연락처' value={presidentPhoneNumber} />
-      )}
+      )} */}
       <ClubInfoRow icon={<BiListUl />} label='동방 위치' value={location} />
       <ClubInfoRow
         icon={<PiCalendarDotsDuotone />}
